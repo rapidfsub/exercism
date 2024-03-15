dbd4d5859a053e3a3c4e66f12a39d15197f7c7ce
Minsub Kim <rapidfsub@gmail.com>
2024-03-15 14:52:14 +0900

    solve language list

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -1,25 +1,25 @@
 defmodule LanguageList do
   def new() do
-    # Please implement the new/0 function
+    []
   end
 
   def add(list, language) do
-    # Please implement the add/2 function
+    list |> List.insert_at(0, language)
   end
 
   def remove(list) do
-    # Please implement the remove/1 function
+    list |> List.pop_at(0) |> elem(1)
   end
 
   def first(list) do
-    # Please implement the first/1 function
+    list |> List.first()
   end
 
   def count(list) do
-    # Please implement the count/1 function
+    list |> length()
   end
 
   def functional_list?(list) do
-    # Please implement the functional_list?/1 function
+    list |> Enum.member?("Elixir")
   end
 end
