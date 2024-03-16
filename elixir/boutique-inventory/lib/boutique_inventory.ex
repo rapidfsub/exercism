3dfc6860877ceabbf5b8cc018ceea18b56f79acf
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 22:44:26 +0900

    solve boutique inventory

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,21 +1,29 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    inventory |> Enum.sort_by(& &1.price)
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    inventory |> Enum.reject(& &1.price)
   end
 
   def update_names(inventory, old_word, new_word) do
-    # Please implement the update_names/3 function
+    inventory
+    |> Enum.map(fn item ->
+      item |> Map.update!(:name, &String.replace(&1, old_word, new_word))
+    end)
   end
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    item
+    |> Map.update!(:quantity_by_size, fn quantities ->
+      quantities |> Map.new(fn {size, quantity} -> {size, quantity + count} end)
+    end)
   end
 
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item.quantity_by_size
+    |> Map.values()
+    |> Enum.sum()
   end
 end
