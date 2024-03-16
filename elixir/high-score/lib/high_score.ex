e1ee57fe3b511bd03195fbdb8f0947e3f920e995
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 12:49:46 +0900

    refactor high score using module attribute

--- elixir/high-score/lib/high_score.ex
+++ elixir/high-score/lib/high_score.ex
@@ -1,9 +1,11 @@
 defmodule HighScore do
+  @initial_score 0
+
   def new() do
     %{}
   end
 
-  def add_player(scores, name, score \\ 0) do
+  def add_player(scores, name, score \\ @initial_score) when not is_map_key(scores, name) do
     scores |> Map.put(name, score)
   end
 
@@ -12,7 +14,7 @@ defmodule HighScore do
   end
 
   def reset_score(scores, name) do
-    scores |> add_player(name)
+    scores |> Map.put(name, @initial_score)
   end
 
   def update_score(scores, name, score) do
