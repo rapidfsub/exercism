7d3c8875fa912470cb973824533f18e46393089e
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 16:48:36 +0900

    solve library fees

--- elixir/library-fees/lib/library_fees.ex
+++ elixir/library-fees/lib/library_fees.ex
@@ -1,25 +1,52 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    string |> NaiveDateTime.from_iso8601!()
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    datetime
+    |> NaiveDateTime.to_time()
+    |> Time.compare(~T[12:00:00]) == :lt
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    offset =
+      if checkout_datetime |> before_noon?() do
+        28
+      else
+        29
+      end
+
+    checkout_datetime
+    |> NaiveDateTime.to_date()
+    |> Date.add(offset)
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    actual_return_datetime
+    |> NaiveDateTime.to_date()
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    datetime |> Date.day_of_week() == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    return = return |> datetime_from_string()
+
+    result =
+      checkout
+      |> datetime_from_string()
+      |> return_date()
+      |> days_late(return)
+      |> Kernel.*(rate)
+
+    if return |> monday?() do
+      result |> div(2)
+    else
+      result
+    end
   end
 end
