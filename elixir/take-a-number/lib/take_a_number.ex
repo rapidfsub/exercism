6dda878da456d6e1799f0068f5475f0287f18184
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 14:49:40 +0900

    solve take a number

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,18 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> run(0) end)
+  end
+
+  defp run(number) when is_integer(number) do
+    receive do
+      {:report_state, pid} -> send(pid, number)
+      {:take_a_number, pid} -> send(pid, number + 1)
+      :stop -> nil
+      _ -> number
+    end
+    |> case do
+      nil -> nil
+      next_state -> next_state |> run()
+    end
   end
 end
