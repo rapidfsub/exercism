866d28fbf064918d1eab0866bc85fd63f3593403
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 15:13:42 +0900

    solve paint by number

--- elixir/paint-by-number/lib/paint_by_number.ex
+++ elixir/paint-by-number/lib/paint_by_number.ex
@@ -1,29 +1,48 @@
 defmodule PaintByNumber do
   def palette_bit_size(color_count) do
-    # Please implement the palette_bit_size/1 function
+    do_palette_bit_size(color_count, 1)
+  end
+
+  defp do_palette_bit_size(color_count, exp) do
+    if Integer.pow(2, exp) < color_count do
+      do_palette_bit_size(color_count, exp + 1)
+    else
+      exp
+    end
   end
 
   def empty_picture() do
-    # Please implement the empty_picture/0 function
+    <<>>
   end
 
   def test_picture() do
-    # Please implement the test_picture/0 function
+    <<0::2, 1::2, 2::2, 3::2>>
   end
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    n = palette_bit_size(color_count)
+    <<pixel_color_index::size(n), picture::bitstring>>
   end
 
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    picture |> split_first_pixel(color_count) |> elem(0)
+  end
+
+  defp split_first_pixel(<<>>, _color_count) do
+    {nil, <<>>}
+  end
+
+  defp split_first_pixel(picture, color_count) do
+    n = palette_bit_size(color_count)
+    <<first::size(n), rest::bitstring>> = picture
+    {first, rest}
   end
 
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    picture |> split_first_pixel(color_count) |> elem(1)
   end
 
   def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
+    <<picture1::bitstring, picture2::bitstring>>
   end
 end
