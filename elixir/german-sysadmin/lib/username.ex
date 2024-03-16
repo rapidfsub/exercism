bccd4d9341ba0cad09c006701e231dd12eafcf3c
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 13:34:50 +0900

    solve german sysadmin

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,13 @@
 defmodule Username do
   def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
-
-    # Please implement the sanitize/1 function
+    username |> Enum.flat_map(&do_sanitize/1)
   end
+
+  def do_sanitize(letter) when letter in ?a..?z, do: [letter]
+  def do_sanitize(?ä), do: ~c"ae"
+  def do_sanitize(?ö), do: ~c"oe"
+  def do_sanitize(?ü), do: ~c"ue"
+  def do_sanitize(?ß), do: ~c"ss"
+  def do_sanitize(?_), do: [?_]
+  def do_sanitize(_letter), do: []
 end
