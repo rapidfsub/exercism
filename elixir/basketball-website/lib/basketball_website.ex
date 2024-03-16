e7ab06dc73f7d1221a27340d9331b7b6a43233c2
Minsub Kim <rapidfsub@gmail.com>
2024-03-16 22:13:38 +0900

    solve basketball website

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,21 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    path
+    |> parse_path!()
+    |> Enum.reduce_while(data, fn key, data ->
+      case data |> Map.get(key) do
+        nil -> {:halt, nil}
+        value -> {:cont, value}
+      end
+    end)
+  end
+
+  defp parse_path!(path) when is_binary(path) do
+    path |> String.split(".")
   end
 
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    parsed_path = path |> parse_path!()
+    data |> get_in(parsed_path)
   end
 end
