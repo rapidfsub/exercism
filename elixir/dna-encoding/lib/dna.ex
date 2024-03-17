81ce01d878ebb283a69340f0fe51d0470469c241
Minsub Kim <rapidfsub@gmail.com>
2024-03-17 11:58:52 +0900

    solve dna encoding

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,17 +1,26 @@
 defmodule DNA do
-  def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
-  end
+  def encode_nucleotide(?\s), do: 0b0000
+  def encode_nucleotide(?A), do: 0b0001
+  def encode_nucleotide(?C), do: 0b0010
+  def encode_nucleotide(?G), do: 0b0100
+  def encode_nucleotide(?T), do: 0b1000
 
-  def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
-  end
+  def decode_nucleotide(0b0000), do: ?\s
+  def decode_nucleotide(0b0001), do: ?A
+  def decode_nucleotide(0b0010), do: ?C
+  def decode_nucleotide(0b0100), do: ?G
+  def decode_nucleotide(0b1000), do: ?T
 
   def encode(dna) do
-    # Please implement the encode/1 function
+    dna
+    |> Enum.map(&encode_nucleotide/1)
+    |> Enum.map(&<<&1::4>>)
+    |> Enum.reduce(&<<&2::bitstring, &1::bitstring>>)
   end
 
   def decode(dna) do
-    # Please implement the decode/1 function
+    for <<code::4 <- dna>> do
+      decode_nucleotide(code)
+    end
   end
 end
