2db67cab56f4481a987be4af4eb160147cf0ed22
Minsub Kim <rapidfsub@gmail.com>
2024-03-17 15:29:14 +0900

    solve community garden

--- elixir/community-garden/lib/community_garden.ex
+++ elixir/community-garden/lib/community_garden.ex
@@ -1,27 +1,57 @@
 # Use the Plot struct as it is provided
 defmodule Plot do
   @enforce_keys [:plot_id, :registered_to]
-  defstruct [:plot_id, :registered_to]
+  defstruct @enforce_keys
 end
 
 defmodule CommunityGarden do
-  def start(opts) do
-    # Please implement the start/1 function
+  defmodule State do
+    @enforce_keys [:plots, :curr_id]
+    defstruct @enforce_keys
+
+    def new() do
+      %__MODULE__{plots: [], curr_id: 0}
+    end
+
+    def get_plots(%__MODULE__{} = state) do
+      state.plots
+    end
+
+    def register(%__MODULE__{} = state, register_to) do
+      next_id = state.curr_id + 1
+      plot = %Plot{plot_id: next_id, registered_to: register_to}
+      {plot, %{state | plots: [plot | state.plots], curr_id: next_id}}
+    end
+
+    def release(%__MODULE__{} = state, plot_id) do
+      %{state | plots: state.plots |> Enum.reject(&(&1.plot_id == plot_id))}
+    end
+
+    def get_registration(%__MODULE__{} = state, plot_id) do
+      case state.plots |> Enum.find(&(&1.plot_id == plot_id)) do
+        nil -> {:not_found, "plot is unregistered"}
+        plot -> plot
+      end
+    end
+  end
+
+  def start(opts \\ []) do
+    Agent.start(&State.new/0, opts)
   end
 
   def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
+    pid |> Agent.get(&State.get_plots/1)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    pid |> Agent.get_and_update(&State.register(&1, register_to))
   end
 
   def release(pid, plot_id) do
-    # Please implement the release/2 function
+    pid |> Agent.update(&State.release(&1, plot_id))
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+    pid |> Agent.get(&State.get_registration(&1, plot_id))
   end
 end
