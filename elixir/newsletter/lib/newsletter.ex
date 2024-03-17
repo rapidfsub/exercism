ba855a714d918ca0cc216fd3338c101c8537cea0
Minsub Kim <rapidfsub@gmail.com>
2024-03-17 11:23:56 +0900

    solve newsletter

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,21 +1,31 @@
 defmodule Newsletter do
   def read_emails(path) do
-    # Please implement the read_emails/1 function
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
   end
 
   def open_log(path) do
-    # Please implement the open_log/1 function
+    path |> File.open!([:write])
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    pid |> IO.puts(email)
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
+    pid |> File.close()
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    pid = log_path |> open_log()
+
+    emails_path
+    |> read_emails()
+    |> Enum.each(fn email ->
+      with :ok <- send_fun.(email) do
+        pid |> log_sent_email(email)
+      end
+    end)
   end
 end
