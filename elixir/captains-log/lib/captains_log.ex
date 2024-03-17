978f8cc475fd841885422282a63e5515b32211ca
Minsub Kim <rapidfsub@gmail.com>
2024-03-17 18:20:37 +0900

    solve captains log

--- elixir/captains-log/lib/captains_log.ex
+++ elixir/captains-log/lib/captains_log.ex
@@ -2,18 +2,24 @@ defmodule CaptainsLog do
   @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
 
   def random_planet_class() do
-    # Please implement the random_planet_class/0 function
+    @planetary_classes |> Enum.random()
   end
 
   def random_ship_registry_number() do
-    # Please implement the random_ship_registry_number/0 function
+    ["NCC", 1000..1999 |> Enum.random()] |> Enum.join("-")
   end
 
   def random_stardate() do
-    # Please implement the random_stardate/0 function
+    41000.0 + :rand.uniform() * 1000.0
   end
 
-  def format_stardate(stardate) do
-    # Please implement the format_stardate/1 function
+  def format_stardate(stardate) when is_float(stardate) do
+    stardate
+    |> Float.round(1)
+    |> to_string()
+  end
+
+  def format_stardate(_stardate) do
+    raise ArgumentError
   end
 end
