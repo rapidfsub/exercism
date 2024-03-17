7265f2c62e78681d0d29cd9d3c8220fd1f02ded1
Minsub Kim <rapidfsub@gmail.com>
2024-03-17 11:04:35 +0900

    solve top secret

--- elixir/top-secret/lib/top_secret.ex
+++ elixir/top-secret/lib/top_secret.ex
@@ -1,13 +1,41 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    string |> Code.string_to_quoted!()
   end
 
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    case ast |> do_decode() do
+      nil -> {ast, acc}
+      message -> {ast, [message | acc]}
+    end
+  end
+
+  defp do_decode({key, _, [{:when, _, [{msg, _, args} | _]} | _]}) when key in [:def, :defp] do
+    do_decode(msg, args)
+  end
+
+  defp do_decode({key, _, [{msg, _, args} | _]}) when key in [:def, :defp] do
+    do_decode(msg, args)
+  end
+
+  defp do_decode(_ast) do
+    nil
+  end
+
+  defp do_decode(msg, args) do
+    msg
+    |> to_string()
+    |> String.graphemes()
+    |> Enum.take(length(args || []))
+    |> to_string()
   end
 
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    string
+    |> to_ast()
+    |> Macro.prewalk([], &decode_secret_message_part/2)
+    |> elem(1)
+    |> Enum.reverse()
+    |> to_string()
   end
 end
