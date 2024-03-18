29e5288c02216dca302ca01ee18fa2cbf98fb973
Minsub Kim <rapidfsub@gmail.com>
2024-03-18 23:40:01 +0900

    refactor take a number deluxe overloading Kernel.def/2

--- elixir/take-a-number-deluxe/lib/take_a_number_deluxe.ex
+++ elixir/take-a-number-deluxe/lib/take_a_number_deluxe.ex
@@ -1,7 +1,32 @@
-require AutoShutdownTimeout
+defmodule AutoShutdownTimeout do
+  defmacro def(call, expr) do
+    expr =
+      expr
+      |> Macro.prewalk(fn
+        {:^, _, [{e1, state}]} ->
+          quote do
+            {unquote(e1), unquote(state), unquote(state).auto_shutdown_timeout}
+          end
+
+        {:^, _, [{:{}, _, [e1, e2, state]}]} ->
+          quote do
+            {unquote(e1), unquote(e2), unquote(state), unquote(state).auto_shutdown_timeout}
+          end
+
+        ast ->
+          ast
+      end)
+
+    quote do
+      Kernel.def(unquote(call), unquote(expr))
+    end
+  end
+end
 
 defmodule TakeANumberDeluxe do
   alias TakeANumberDeluxe.State
+  import Kernel, except: [def: 2]
+  import AutoShutdownTimeout, only: [def: 2]
   use GenServer
 
   # Client API
@@ -47,35 +72,35 @@ defmodule TakeANumberDeluxe do
       end
 
     with {:ok, state} <- result do
-      @{:ok, state}
+      ^{:ok, state}
     end
   end
 
   @impl GenServer
   def handle_call(:report_state, _from, state) do
-    @{:reply, state, state}
+    ^{:reply, state, state}
   end
 
   @impl GenServer
   def handle_call(:queue_new_number, _from, state) do
     case state |> State.queue_new_number() do
-      {:ok, num, new_state} -> @{:reply, {:ok, num}, new_state}
-      {:error, _reason} = error -> @{:reply, error, state}
+      {:ok, num, new_state} -> ^{:reply, {:ok, num}, new_state}
+      {:error, _reason} = error -> ^{:reply, error, state}
     end
   end
 
   @impl GenServer
   def handle_call({:serve_next_queued_number, priority}, _from, state) do
     case state |> State.serve_next_queued_number(priority) do
-      {:ok, num, new_state} -> @{:reply, {:ok, num}, new_state}
-      {:error, _reason} = error -> @{:reply, error, state}
+      {:ok, num, new_state} -> ^{:reply, {:ok, num}, new_state}
+      {:error, _reason} = error -> ^{:reply, error, state}
     end
   end
 
   @impl GenServer
   def handle_call(:reset_state, _from, state) do
     {:ok, state} = State.new(state.min_number, state.max_number, state.auto_shutdown_timeout)
-    @{:reply, :ok, state}
+    ^{:reply, :ok, state}
   end
 
   @impl GenServer
@@ -85,7 +110,6 @@ defmodule TakeANumberDeluxe do
 
   @impl GenServer
   def handle_info(_msg, state) do
-    @{:noreply, state}
+    ^{:noreply, state}
   end
 end
-|> AutoShutdownTimeout.append_timeout()
