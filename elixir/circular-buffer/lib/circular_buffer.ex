e999be2f271d6d9e3fe36c03686928a5b9de26ce
Minsub Kim <rapidfsub@gmail.com>
2024-03-19 08:33:51 +0900

    solve circular buffer

--- elixir/circular-buffer/lib/circular_buffer.ex
+++ elixir/circular-buffer/lib/circular_buffer.ex
@@ -3,11 +3,53 @@ defmodule CircularBuffer do
   An API to a stateful process that fills and empties a circular buffer
   """
 
+  defmodule State do
+    @enforce_keys [:capacity, :queue]
+    defstruct @enforce_keys
+
+    def new(capacity) when capacity > 0 do
+      %__MODULE__{capacity: capacity, queue: :queue.new()}
+    end
+
+    def read(%__MODULE__{} = state) do
+      case state.queue |> :queue.out() do
+        {{:value, value}, queue} -> {:ok, {value, %{state | queue: queue}}}
+        {:empty, _queue} -> {:error, :empty}
+      end
+    end
+
+    def write(%__MODULE__{} = state, item) do
+      if state.queue |> :queue.len() < state.capacity do
+        {:ok, %{state | queue: :queue.in(item, state.queue)}}
+      else
+        {:error, :full}
+      end
+    end
+
+    def overwrite(%__MODULE__{} = state, item) do
+      queue =
+        if state.queue |> :queue.len() < state.capacity do
+          state.queue
+        else
+          state.queue |> :queue.drop()
+        end
+
+      %{state | queue: :queue.in(item, queue)}
+    end
+
+    def clear(%__MODULE__{} = state) do
+      %{state | queue: :queue.new()}
+    end
+  end
+
+  use GenServer
+
   @doc """
   Create a new buffer of a given capacity
   """
   @spec new(capacity :: integer) :: {:ok, pid}
   def new(capacity) do
+    __MODULE__ |> GenServer.start(capacity)
   end
 
   @doc """
@@ -15,6 +57,7 @@ defmodule CircularBuffer do
   """
   @spec read(buffer :: pid) :: {:ok, any} | {:error, atom}
   def read(buffer) do
+    buffer |> GenServer.call(:read)
   end
 
   @doc """
@@ -22,6 +65,7 @@ defmodule CircularBuffer do
   """
   @spec write(buffer :: pid, item :: any) :: :ok | {:error, atom}
   def write(buffer, item) do
+    buffer |> GenServer.call({:write, item})
   end
 
   @doc """
@@ -29,6 +73,7 @@ defmodule CircularBuffer do
   """
   @spec overwrite(buffer :: pid, item :: any) :: :ok
   def overwrite(buffer, item) do
+    buffer |> GenServer.call({:overwrite, item})
   end
 
   @doc """
@@ -36,5 +81,37 @@ defmodule CircularBuffer do
   """
   @spec clear(buffer :: pid) :: :ok
   def clear(buffer) do
+    buffer |> GenServer.call(:clear)
+  end
+
+  @impl GenServer
+  def init(init_arg) do
+    {:ok, State.new(init_arg)}
+  end
+
+  @impl GenServer
+  def handle_call(:read, _from, state) do
+    case state |> State.read() do
+      {:ok, {value, new_state}} -> {:reply, {:ok, value}, new_state}
+      {:error, _reason} = error -> {:reply, error, state}
+    end
+  end
+
+  @impl GenServer
+  def handle_call({:write, item}, _from, state) do
+    case state |> State.write(item) do
+      {:ok, new_state} -> {:reply, :ok, new_state}
+      {:error, _reason} = error -> {:reply, error, state}
+    end
+  end
+
+  @impl GenServer
+  def handle_call({:overwrite, item}, _from, state) do
+    {:reply, :ok, state |> State.overwrite(item)}
+  end
+
+  @impl GenServer
+  def handle_call(:clear, _from, state) do
+    {:reply, :ok, state |> State.clear()}
   end
 end
