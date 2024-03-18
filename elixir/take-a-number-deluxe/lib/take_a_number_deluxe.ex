e34aa0c1451afae4a6b4a74d3409dea87b29a4e6
Minsub Kim <rapidfsub@gmail.com>
2024-03-18 22:45:09 +0900

    refactor take a number deluxe using macro

--- elixir/take-a-number-deluxe/lib/take_a_number_deluxe.ex
+++ elixir/take-a-number-deluxe/lib/take_a_number_deluxe.ex
@@ -1,3 +1,5 @@
+require AutoShutdownTimeout
+
 defmodule TakeANumberDeluxe do
   alias TakeANumberDeluxe.State
   use GenServer
@@ -33,43 +35,47 @@ defmodule TakeANumberDeluxe do
 
   @impl GenServer
   def init(init_arg) do
-    with min_n <- init_arg |> Keyword.get(:min_number),
-         max_n <- init_arg |> Keyword.get(:max_number),
-         timeout <- init_arg |> Keyword.get(:auto_shutdown_timeout, :infinity),
-         {:ok, state} <- State.new(min_n, max_n, timeout) do
-      {:ok, state} |> append_timeout()
-    else
-      _ -> {:error, :invalid_configuration}
+    min_n = init_arg |> Keyword.get(:min_number)
+    max_n = init_arg |> Keyword.get(:max_number)
+    timeout = init_arg |> Keyword.get(:auto_shutdown_timeout)
+
+    result =
+      if timeout do
+        State.new(min_n, max_n, timeout)
+      else
+        State.new(min_n, max_n)
+      end
+
+    with {:ok, state} <- result do
+      @{:ok, state}
     end
   end
 
   @impl GenServer
   def handle_call(:report_state, _from, state) do
-    {:reply, state, state} |> append_timeout()
+    @{:reply, state, state}
   end
 
   @impl GenServer
   def handle_call(:queue_new_number, _from, state) do
     case state |> State.queue_new_number() do
-      {:ok, num, new_state} -> {:reply, {:ok, num}, new_state}
-      {:error, _reason} = error -> {:reply, error, state}
+      {:ok, num, new_state} -> @{:reply, {:ok, num}, new_state}
+      {:error, _reason} = error -> @{:reply, error, state}
     end
-    |> append_timeout()
   end
 
   @impl GenServer
   def handle_call({:serve_next_queued_number, priority}, _from, state) do
     case state |> State.serve_next_queued_number(priority) do
-      {:ok, num, new_state} -> {:reply, {:ok, num}, new_state}
-      {:error, _reason} = error -> {:reply, error, state}
+      {:ok, num, new_state} -> @{:reply, {:ok, num}, new_state}
+      {:error, _reason} = error -> @{:reply, error, state}
     end
-    |> append_timeout()
   end
 
   @impl GenServer
   def handle_call(:reset_state, _from, state) do
     {:ok, state} = State.new(state.min_number, state.max_number, state.auto_shutdown_timeout)
-    {:reply, :ok, state} |> append_timeout()
+    @{:reply, :ok, state}
   end
 
   @impl GenServer
@@ -79,14 +85,7 @@ defmodule TakeANumberDeluxe do
 
   @impl GenServer
   def handle_info(_msg, state) do
-    {:noreply, state} |> append_timeout()
-  end
-
-  defp append_timeout(tuple) do
-    case tuple do
-      {_, %State{} = state} -> tuple |> Tuple.append(state.auto_shutdown_timeout)
-      {_, _, %State{} = state} -> tuple |> Tuple.append(state.auto_shutdown_timeout)
-      _ -> tuple
-    end
+    @{:noreply, state}
   end
 end
+|> AutoShutdownTimeout.append_timeout()
