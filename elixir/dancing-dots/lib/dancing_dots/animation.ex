3910494134bec2b9702c6eccbf7d3ce74d7d7557
Minsub Kim <rapidfsub@gmail.com>
2024-03-18 09:25:52 +0900

    download rpn calculator inspection

--- elixir/dancing-dots/lib/dancing_dots/animation.ex
+++ elixir/dancing-dots/lib/dancing_dots/animation.ex
@@ -4,13 +4,54 @@ defmodule DancingDots.Animation do
   @type error :: any
   @type frame_number :: pos_integer
 
-  # Please implement the module
+  @callback init(opts :: opts()) :: {:ok, opts()} | {:error, error()}
+  @callback handle_frame(dot :: dot(), frame_number :: frame_number(), opts :: opts()) :: dot()
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour DancingDots.Animation
+
+      @impl DancingDots.Animation
+      def init(opts) do
+        {:ok, opts}
+      end
+
+      defoverridable init: 1
+    end
+  end
 end
 
 defmodule DancingDots.Flicker do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, _opts) do
+    if frame_number |> rem(4) == 0 do
+      dot |> Map.update!(:opacity, &(&1 / 2))
+    else
+      dot
+    end
+  end
 end
 
 defmodule DancingDots.Zoom do
-  # Please implement the module
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init(opts) do
+    case opts |> Keyword.get(:velocity) do
+      velocity when is_number(velocity) ->
+        {:ok, opts}
+
+      velocity ->
+        {:error,
+         "The :velocity option is required, and its value must be a number. Got: #{inspect(velocity)}"}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    velocity = opts |> Keyword.fetch!(:velocity)
+    dot |> Map.update!(:radius, &(&1 + (frame_number - 1) * velocity))
+  end
 end
