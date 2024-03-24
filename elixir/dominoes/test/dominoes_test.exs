3af9109ed903a3743d3b1f0501536f8483003904
Minsub Kim <rapidfsub@gmail.com>
2024-03-24 20:00:44 +0900

    solve dominoes

--- elixir/dominoes/test/dominoes_test.exs
+++ elixir/dominoes/test/dominoes_test.exs
@@ -5,53 +5,53 @@ defmodule DominoesTest do
     assert Dominoes.chain?([]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "singleton input = singleton output" do
     assert Dominoes.chain?([{1, 1}]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "singleton that can't be chained" do
     assert Dominoes.chain?([{1, 2}]) == false
   end
 
-  @tag :pending
+  # @tag :pending
   test "three elements" do
     assert Dominoes.chain?([{1, 2}, {3, 1}, {2, 3}]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "can reverse dominoes" do
     assert Dominoes.chain?([{1, 2}, {1, 3}, {2, 3}]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "can't be chained" do
     assert Dominoes.chain?([{1, 2}, {4, 1}, {2, 3}]) == false
   end
 
-  @tag :pending
+  # @tag :pending
   test "disconnected - double loop" do
     assert Dominoes.chain?([{1, 2}, {2, 1}, {3, 4}, {4, 3}]) == false
   end
 
-  @tag :pending
+  # @tag :pending
   test "disconnected - single isolated" do
     assert Dominoes.chain?([{1, 2}, {2, 3}, {3, 1}, {4, 4}]) == false
   end
 
-  @tag :pending
+  # @tag :pending
   test "need backtrack" do
     # a variation in which we have to turn but no duplicates
     assert Dominoes.chain?([{1, 2}, {2, 3}, {3, 1}, {2, 4}, {2, 4}]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "separate loops" do
     assert Dominoes.chain?([{1, 2}, {2, 3}, {3, 1}, {1, 1}, {2, 2}, {3, 3}]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "nine elements" do
     assert Dominoes.chain?([
              {1, 2},
@@ -66,22 +66,22 @@ defmodule DominoesTest do
            ]) == true
   end
 
-  @tag :pending
+  # @tag :pending
   test "separate three-domino loops" do
     refute Dominoes.chain?([{1, 2}, {2, 3}, {3, 1}, {4, 5}, {5, 6}, {6, 4}])
   end
 
-  @tag :pending
+  # @tag :pending
   test "disconnected - simple" do
     refute Dominoes.chain?([{1, 1}, {2, 2}])
   end
 
-  @tag :pending
+  # @tag :pending
   test "first and last not matching" do
     assert Dominoes.chain?([{1, 2}, {2, 3}, {3, 4}]) == false
   end
 
-  @tag :pending
+  # @tag :pending
   test "wrong starting order" do
     assert Dominoes.chain?([{2, 1}, {2, 3}, {3, 1}]) == true
   end
