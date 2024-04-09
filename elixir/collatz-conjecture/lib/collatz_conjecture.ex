781dbd00c9e3d168a206e4f4c608c9353944287b
Minsub Kim <rapidfsub@gmail.com>
2024-04-09 17:26:25 +0900

    solve collatz conjecture

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -1,4 +1,6 @@
 defmodule CollatzConjecture do
+  import Integer, only: [is_even: 1]
+
   @doc """
   calc/1 takes an integer and returns the number of steps required to get the
   number to 1 when following the rules:
@@ -6,6 +8,15 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) do
+  def calc(1) do
+    0
+  end
+
+  def calc(input) when input |> is_integer() and input > 1 do
+    if input |> is_even() do
+      input |> div(2) |> calc()
+    else
+      (input * 3 + 1) |> calc()
+    end + 1
   end
 end
