641be8371c03c58ff23ec0575784b4f7e74940c0
Minsub Kim <rapidfsub@gmail.com>
2024-04-09 10:54:57 +0900

    fix warning

--- elixir/all-your-base/lib/all_your_base.ex
+++ elixir/all-your-base/lib/all_your_base.ex
@@ -5,11 +5,11 @@ defmodule AllYourBase do
   """
 
   @spec convert(list, integer, integer) :: {:ok, list} | {:error, String.t()}
-  def convert(digits, input_base, output_base) when input_base < 2 do
+  def convert(_digits, input_base, _output_base) when input_base < 2 do
     {:error, "input base must be >= 2"}
   end
 
-  def convert(digits, input_base, output_base) when output_base < 2 do
+  def convert(_digits, _input_base, output_base) when output_base < 2 do
     {:error, "output base must be >= 2"}
   end
 
