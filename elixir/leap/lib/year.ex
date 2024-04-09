d12437d7d3445f7d6d48a34064f9c3279813af68
Minsub Kim <rapidfsub@gmail.com>
2024-04-09 17:33:10 +0900

    solve leap

--- elixir/leap/lib/year.ex
+++ elixir/leap/lib/year.ex
@@ -9,6 +9,7 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-  end
+  def leap_year?(year) when year |> rem(4) != 0, do: false
+  def leap_year?(year) when year |> rem(100) != 0, do: true
+  def leap_year?(year), do: year |> rem(400) == 0
 end
