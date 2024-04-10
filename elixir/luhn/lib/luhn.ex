0baf13bae48d629804355fec980a2503e724159d
Minsub Kim <rapidfsub@gmail.com>
2024-04-10 11:55:28 +0900

    solve luhn

--- elixir/luhn/lib/luhn.ex
+++ elixir/luhn/lib/luhn.ex
@@ -1,8 +1,37 @@
 defmodule Luhn do
+  import Integer, only: [is_odd: 1]
+
   @doc """
   Checks if the given number is valid via the luhn formula
   """
   @spec valid?(String.t()) :: boolean
-  def valid?(number) do
+  def valid?(number) when number |> is_binary() do
+    if number |> String.match?(~r/[^\p{N}\p{Zs}]/) do
+      false
+    else
+      ~r/\p{N}/
+      |> Regex.scan(number)
+      |> Enum.map(fn [letter] -> letter |> String.to_integer() end)
+      |> case do
+        [0] ->
+          false
+
+        digits ->
+          digits
+          |> Enum.reverse()
+          |> Enum.with_index(fn digit, index ->
+            if index |> is_odd() do
+              case digit * 2 do
+                result when result < 10 -> result
+                result -> result - 9
+              end
+            else
+              digit
+            end
+          end)
+          |> Enum.sum()
+          |> rem(10) == 0
+      end
+    end
   end
 end
