37d9a5c6bb8cd0e0726dad01ab59b242fd986ea5
Minsub Kim <rapidfsub@gmail.com>
2024-04-10 21:51:45 +0900

    solve change

--- elixir/change/lib/change.ex
+++ elixir/change/lib/change.ex
@@ -17,5 +17,24 @@ defmodule Change do
 
   @spec generate(list, integer) :: {:ok, list} | {:error, String.t()}
   def generate(coins, target) do
+    coins
+    |> Enum.sort(:desc)
+    |> do_generate(target, [], 0, target + 1)
+    |> case do
+      nil -> {:error, "cannot change"}
+      {result, _size} -> {:ok, result}
+    end
+  end
+
+  defp do_generate(_coins, 0, acc, step, _size), do: {acc, step}
+  defp do_generate([], _target, _acc, _step, _size), do: nil
+
+  defp do_generate([head | tail] = coins, target, acc, step, size) do
+    if target > 0 and step < size do
+      case coins |> do_generate(target - head, [head | acc], step + 1, size) do
+        {lhs, lsize} -> tail |> do_generate(target, acc, step, lsize) || {lhs, lsize}
+        nil -> tail |> do_generate(target, acc, step, size)
+      end
+    end
   end
 end
