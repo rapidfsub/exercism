0316bb40de218961e17066c160607dc5ef0072cd
Minsub Kim <rapidfsub@gmail.com>
2024-04-11 11:58:42 +0900

    solve sublist

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -4,5 +4,24 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
+    case {a |> sublist?(b), b |> sublist?(a)} do
+      {true, true} -> :equal
+      {true, false} -> :sublist
+      {false, true} -> :superlist
+      {false, false} -> :unequal
+    end
+  end
+
+  defp sublist?(a, b) do
+    len = a |> length()
+
+    b
+    |> Stream.unfold(fn l ->
+      case l |> Enum.take(len) do
+        element when element |> length() < len -> nil
+        element -> {element, l |> Enum.drop(1)}
+      end
+    end)
+    |> Enum.any?(&(&1 === a))
   end
 end
