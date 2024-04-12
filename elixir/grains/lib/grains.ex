c41c00fab85034f9c74ea13a51440e795eeea7bf
Minsub Kim <rapidfsub@gmail.com>
2024-04-12 09:54:47 +0900

    solve grains

--- elixir/grains/lib/grains.ex
+++ elixir/grains/lib/grains.ex
@@ -3,13 +3,20 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
-  def square(number) do
-  end
+  def square(number) when number in 1..64, do: {:ok, 2 ** (number - 1)}
+  def square(_number), do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
 
   @doc """
   Adds square of each number from 1 to 64.
   """
   @spec total :: {:ok, pos_integer()}
-  def total do
+  def total() do
+    total =
+      for number <- 1..64, {:ok, count} = number |> square() do
+        count
+      end
+      |> Enum.sum()
+
+    {:ok, total}
   end
 end
