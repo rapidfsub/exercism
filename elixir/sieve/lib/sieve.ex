8317b9d53dbd099dc5ad372f451f5dbd422ec9d9
Minsub Kim <rapidfsub@gmail.com>
2024-04-12 09:22:45 +0900

    solve sieve

--- elixir/sieve/lib/sieve.ex
+++ elixir/sieve/lib/sieve.ex
@@ -4,5 +4,23 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
+    2..limit//1
+    |> Enum.to_list()
+    |> do_primes_to(limit, [], MapSet.new())
+  end
+
+  defp do_primes_to([], _limit, acc, _did_visit) do
+    acc |> Enum.reverse()
+  end
+
+  defp do_primes_to([head | tail], limit, acc, did_visit) do
+    {acc, did_visit} =
+      if did_visit |> MapSet.member?(head) do
+        {acc, did_visit}
+      else
+        {[head | acc], head..limit//head |> Enum.into(did_visit)}
+      end
+
+    tail |> do_primes_to(limit, acc, did_visit)
   end
 end
