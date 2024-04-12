9a47d2ac4dc6964ad8c908fdadc97d6015987731
Minsub Kim <rapidfsub@gmail.com>
2024-04-12 19:45:22 +0900

    solve gigasecond

--- elixir/gigasecond/lib/gigasecond.ex
+++ elixir/gigasecond/lib/gigasecond.ex
@@ -5,5 +5,11 @@ defmodule Gigasecond do
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
           {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
   def from({{year, month, day}, {hours, minutes, seconds}}) do
+    dt =
+      Date.new!(year, month, day)
+      |> DateTime.new!(Time.new!(hours, minutes, seconds))
+      |> DateTime.add(1_000_000_000)
+
+    {{dt.year, dt.month, dt.day}, {dt.hour, dt.minute, dt.second}}
   end
 end
