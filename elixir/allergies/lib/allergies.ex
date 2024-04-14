6fe425fbfd7fd2148d2bb6ee58c2bf7c6a139a8c
Minsub Kim <rapidfsub@gmail.com>
2024-04-14 22:04:12 +0900

    solve allergies

--- elixir/allergies/lib/allergies.ex
+++ elixir/allergies/lib/allergies.ex
@@ -4,6 +4,10 @@ defmodule Allergies do
   """
   @spec list(non_neg_integer) :: [String.t()]
   def list(flags) do
+    Stream.unfold(1, &{&1, &1 * 2})
+    |> Enum.take(8)
+    |> Enum.filter(&(Bitwise.band(flags, &1) > 0))
+    |> Enum.map(&from_flag/1)
   end
 
   @doc """
@@ -11,5 +15,15 @@ defmodule Allergies do
   """
   @spec allergic_to?(non_neg_integer, String.t()) :: boolean
   def allergic_to?(flags, item) do
+    item in list(flags)
   end
+
+  defp from_flag(0x01), do: "eggs"
+  defp from_flag(0x02), do: "peanuts"
+  defp from_flag(0x04), do: "shellfish"
+  defp from_flag(0x08), do: "strawberries"
+  defp from_flag(0x10), do: "tomatoes"
+  defp from_flag(0x20), do: "chocolate"
+  defp from_flag(0x40), do: "pollen"
+  defp from_flag(0x80), do: "cats"
 end
