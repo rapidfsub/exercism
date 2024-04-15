13795edafb26432e32c719fe863a8a4aabd49174
Minsub Kim <rapidfsub@gmail.com>
2024-04-15 12:51:01 +0900

    solve bob

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,5 +1,15 @@
 defmodule Bob do
   @spec hey(String.t()) :: String.t()
   def hey(input) do
+    input = String.trim(input)
+    shouting = String.match?(input, ~r/\p{L}/u) && String.upcase(input) == input
+    asking = String.ends_with?(input, "?")
+    String.trim(input) |> do_hey(shouting, asking)
   end
+
+  defp do_hey("", _shouting, _asking), do: "Fine. Be that way!"
+  defp do_hey(_input, true, true), do: "Calm down, I know what I'm doing!"
+  defp do_hey(_input, true, false), do: "Whoa, chill out!"
+  defp do_hey(_input, false, true), do: "Sure."
+  defp do_hey(_input, false, false), do: "Whatever."
 end
