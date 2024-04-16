4cc42f1a04745910f20923b9874adbcb32dc8339
Minsub Kim <rapidfsub@gmail.com>
2024-04-16 21:24:03 +0900

    solve meetup

--- elixir/meetup/lib/meetup.ex
+++ elixir/meetup/lib/meetup.ex
@@ -22,5 +22,29 @@ defmodule Meetup do
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: Date.t()
   def meetup(year, month, weekday, schedule) do
+    day_of_week = day_of_week(weekday)
+
+    days =
+      Date.new!(year, month, 1)
+      |> Stream.unfold(&{&1, Date.add(&1, 1)})
+      |> Enum.take_while(&(&1.month == month))
+      |> Enum.filter(&(Date.day_of_week(&1) == day_of_week))
+
+    case schedule do
+      :first -> Enum.fetch!(days, 0)
+      :second -> Enum.fetch!(days, 1)
+      :third -> Enum.fetch!(days, 2)
+      :fourth -> Enum.fetch!(days, 3)
+      :last -> Enum.fetch!(days, -1)
+      :teenth -> Enum.find(days, &(&1.day in 13..19))
+    end
   end
+
+  defp day_of_week(:monday), do: 1
+  defp day_of_week(:tuesday), do: 2
+  defp day_of_week(:wednesday), do: 3
+  defp day_of_week(:thursday), do: 4
+  defp day_of_week(:friday), do: 5
+  defp day_of_week(:saturday), do: 6
+  defp day_of_week(:sunday), do: 7
 end
