e2577b7c3b6e6f9c776cd6a9f427926f1138d841
Minsub Kim <rapidfsub@gmail.com>
2024-04-16 12:33:50 +0900

    solve perfect numbers

--- elixir/perfect-numbers/lib/perfect_numbers.ex
+++ elixir/perfect-numbers/lib/perfect_numbers.ex
@@ -10,6 +10,32 @@ defmodule PerfectNumbers do
   :deficient if the aliquot sum is less than `number`
   """
   @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
-  def classify(number) do
+  def classify(number) when number > 0 do
+    aliquot_sum = prime_factorization(number) |> Enum.sum()
+
+    cond do
+      number < aliquot_sum -> {:ok, :abundant}
+      number > aliquot_sum -> {:ok, :deficient}
+      true -> {:ok, :perfect}
+    end
+  end
+
+  def classify(_number) do
+    {:error, "Classification is only possible for natural numbers."}
+  end
+
+  defp prime_factorization(number) do
+    trunc_sqrt = :math.sqrt(number) |> floor()
+
+    1..trunc_sqrt
+    |> Enum.flat_map(fn candid ->
+      case {div(number, candid), rem(number, candid)} do
+        {^candid, 0} -> [candid]
+        {quot, 0} -> [candid, quot]
+        _ -> []
+      end
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.drop(1)
   end
 end
