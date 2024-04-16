a9417d9cc95eb086661c2debdb432fda878e3cd1
Minsub Kim <rapidfsub@gmail.com>
2024-04-16 12:16:22 +0900

    solve triangle

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -6,5 +6,13 @@ defmodule Triangle do
   """
   @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
   def kind(a, b, c) do
+    Enum.sort([a, b, c]) |> kind()
   end
+
+  defp kind([a, _b, _c]) when a <= 0, do: {:error, "all side lengths must be positive"}
+  defp kind([a, b, c]) when a + b <= c, do: {:error, "side lengths violate triangle inequality"}
+  defp kind([a, a, a]), do: {:ok, :equilateral}
+  defp kind([a, a, _c]), do: {:ok, :isosceles}
+  defp kind([_a, b, b]), do: {:ok, :isosceles}
+  defp kind([_a, _b, _c]), do: {:ok, :scalene}
 end
