1e635262c52a75b35011387342e8079051fa7963
Minsub Kim <rapidfsub@gmail.com>
2024-04-17 12:49:48 +0900

    solve darts

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,13 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    r_square = x ** 2 + y ** 2
+
+    cond do
+      r_square > 100 -> 0
+      r_square > 25 -> 1
+      r_square > 1 -> 5
+      true -> 10
+    end
   end
 end
