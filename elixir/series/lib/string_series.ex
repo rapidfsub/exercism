047890ce35e7927a72ed04e63d09e4414159d552
Minsub Kim <rapidfsub@gmail.com>
2024-04-30 14:40:03 +0900

    solve series

--- elixir/series/lib/string_series.ex
+++ elixir/series/lib/string_series.ex
@@ -5,6 +5,19 @@ defmodule StringSeries do
   return an empty list.
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(s, size) when size > 0 do
+    s
+    |> String.graphemes()
+    |> Stream.unfold(fn letters ->
+      case Enum.take(letters, size) do
+        result when length(result) == size -> {Enum.join(result), Enum.drop(letters, 1)}
+        _ -> nil
+      end
+    end)
+    |> Enum.to_list()
+  end
+
   def slices(_s, _size) do
+    []
   end
 end
