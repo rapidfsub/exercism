ff6991f709f13a6fd57e0cb5287d3887101dd4c6
Minsub Kim <rapidfsub@gmail.com>
2024-04-30 14:25:44 +0900

    solve acronym

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -5,5 +5,9 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    string
+    |> String.split(~r/[^\p{L}']+/u, trim: true)
+    |> Enum.map(fn <<first::binary-size(1), _::binary>> -> String.upcase(first) end)
+    |> Enum.join()
   end
 end
