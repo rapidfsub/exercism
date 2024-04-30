70ec11dcbcef55782c998eca503bc1c524bcdfe8
Minsub Kim <rapidfsub@gmail.com>
2024-04-30 21:26:31 +0900

    solve anagram

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,17 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, &do_match(&1, base))
+  end
+
+  defp do_match(l, r) do
+    case {String.downcase(l), String.downcase(r)} do
+      {word, word} -> false
+      {lhs, rhs} -> letters(lhs) == letters(rhs)
+    end
+  end
+
+  defp letters(word) do
+    String.graphemes(word) |> Enum.sort()
   end
 end
