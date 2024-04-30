1c4fa72368ea422023d1e76f85672febebd92285
Minsub Kim <rapidfsub@gmail.com>
2024-04-30 15:01:43 +0900

    solve word count

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,10 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    sentence = String.downcase(sentence) |> String.replace(~r/[^\p{L}\p{N}']+/u, " ")
+
+    Regex.scan(~r/\b[\p{L}\p{N}']+\b/u, sentence)
+    |> List.flatten()
+    |> Enum.frequencies()
   end
 end
