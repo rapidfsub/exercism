5280b706778e36051a96a16f0795c23736554c27
Minsub Kim <rapidfsub@gmail.com>
2024-05-02 07:21:18 +0900

    solve atbash cipher

--- elixir/atbash-cipher/lib/atbash.ex
+++ elixir/atbash-cipher/lib/atbash.ex
@@ -1,4 +1,8 @@
 defmodule Atbash do
+  @alphabet ?a..?z
+  @numbers ?0..?9
+  @end_index Enum.count(@alphabet) - 1
+
   @doc """
   Encode a given plaintext to the corresponding ciphertext
 
@@ -9,9 +13,25 @@ defmodule Atbash do
   """
   @spec encode(String.t()) :: String.t()
   def encode(plaintext) do
+    plaintext
+    |> transform()
+    |> Enum.chunk_every(5)
+    |> Enum.join(" ")
   end
 
   @spec decode(String.t()) :: String.t()
   def decode(cipher) do
+    transform(cipher) |> to_string()
+  end
+
+  defp transform(text) do
+    text
+    |> String.downcase()
+    |> to_charlist()
+    |> Enum.flat_map(fn
+      l when l in @numbers -> [l]
+      l when l in @alphabet -> [@end_index - l + ?a * 2]
+      _l -> []
+    end)
   end
 end
