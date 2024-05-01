ff390130bf6a9cc383923c107d4865f44da6d67e
Minsub Kim <rapidfsub@gmail.com>
2024-05-01 18:19:13 +0900

    solve run length encoding

--- elixir/run-length-encoding/lib/run_length_encoder.ex
+++ elixir/run-length-encoding/lib/run_length_encoder.ex
@@ -8,9 +8,25 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t()) :: String.t()
   def encode(string) do
+    encode(string, nil, nil)
   end
+
+  defp encode(<<>>, nil, _c), do: ""
+  defp encode(<<>>, l, 1), do: l
+  defp encode(<<>>, l, c), do: to_string(c) <> l
+  defp encode(<<h::binary-1, t::binary>>, nil, _c), do: encode(t, h, 1)
+  defp encode(<<l::binary-1, t::binary>>, l, c), do: encode(t, l, c + 1)
+  defp encode(<<h::binary-1, t::binary>>, l, 1), do: l <> encode(t, h, 1)
+  defp encode(<<h::binary-1, t::binary>>, l, c), do: to_string(c) <> l <> encode(t, h, 1)
 
   @spec decode(String.t()) :: String.t()
   def decode(string) do
+    decode(string, nil)
   end
+
+  defp decode(<<>>, nil), do: ""
+  defp decode(<<h::8, t::binary>>, nil) when h in ?0..?9, do: decode(t, h - ?0)
+  defp decode(<<h::binary-1, t::binary>>, nil), do: h <> decode(t, nil)
+  defp decode(<<h::8, t::binary>>, c) when h in ?0..?9, do: decode(t, c * 10 + h - ?0)
+  defp decode(<<h::binary-1, t::binary>>, c), do: String.duplicate(h, c) <> decode(t, nil)
 end
