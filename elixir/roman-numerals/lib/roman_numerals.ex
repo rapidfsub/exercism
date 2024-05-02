00c31b80608ffd806e22a002e4eec9a8da1b51fd
Minsub Kim <rapidfsub@gmail.com>
2024-05-02 22:37:47 +0900

    solve roman numerals

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -3,6 +3,19 @@ defmodule RomanNumerals do
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
-  def numeral(number) do
+  def numeral(number) when number in 1..3999 do
+    do_numeral(number)
   end
+
+  defp do_numeral(n) when n >= 1000, do: "M" <> do_numeral(n - 1000)
+  defp do_numeral(n) when n >= 900 or n in 400..499, do: "C" <> do_numeral(n + 100)
+  defp do_numeral(n) when n >= 500, do: "D" <> do_numeral(n - 500)
+  defp do_numeral(n) when n >= 100, do: "C" <> do_numeral(n - 100)
+  defp do_numeral(n) when n >= 90 or n in 40..49, do: "X" <> do_numeral(n + 10)
+  defp do_numeral(n) when n >= 50, do: "L" <> do_numeral(n - 50)
+  defp do_numeral(n) when n >= 10, do: "X" <> do_numeral(n - 10)
+  defp do_numeral(n) when n in [4, 9], do: "I" <> do_numeral(n + 1)
+  defp do_numeral(n) when n >= 5, do: "V" <> do_numeral(n - 5)
+  defp do_numeral(n) when n >= 1, do: "I" <> do_numeral(n - 1)
+  defp do_numeral(0), do: ""
 end
