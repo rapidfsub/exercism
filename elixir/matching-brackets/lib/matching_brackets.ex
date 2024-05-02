ff80f916b9273e1c667f1a11c923f5bd664e400c
Minsub Kim <rapidfsub@gmail.com>
2024-05-02 22:48:09 +0900

    solve matching brackets

--- elixir/matching-brackets/lib/matching_brackets.ex
+++ elixir/matching-brackets/lib/matching_brackets.ex
@@ -4,5 +4,14 @@ defmodule MatchingBrackets do
   """
   @spec check_brackets(String.t()) :: boolean
   def check_brackets(str) do
+    String.graphemes(str) |> check_brackets([])
   end
+
+  defp check_brackets([")" | str], ["(" | acc]), do: check_brackets(str, acc)
+  defp check_brackets(["}" | str], ["{" | acc]), do: check_brackets(str, acc)
+  defp check_brackets(["]" | str], ["[" | acc]), do: check_brackets(str, acc)
+  defp check_brackets([h | _t], _acc) when h in ~w/] } )/, do: false
+  defp check_brackets([h | t], acc) when h in ~w/( { [/, do: check_brackets(t, [h | acc])
+  defp check_brackets([_h | t], acc), do: check_brackets(t, acc)
+  defp check_brackets([], acc), do: Enum.empty?(acc)
 end
