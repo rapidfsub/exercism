dc2c0dd520db6e5343cca72004a3ba24eae0c5e9
Minsub Kim <rapidfsub@gmail.com>
2024-05-02 22:59:09 +0900

    solve pascals triangle

--- elixir/pascals-triangle/lib/pascals_triangle.ex
+++ elixir/pascals-triangle/lib/pascals_triangle.ex
@@ -4,6 +4,17 @@ defmodule PascalsTriangle do
   with the given height
   """
   @spec rows(integer) :: [[integer]]
-  def rows(num) do
+  def rows(num) when num > 0 do
+    do_rows(num) |> Enum.reverse()
+  end
+
+  defp do_rows(num) when num > 1 do
+    [prev | _] = triangle = do_rows(num - 1)
+    curr = Enum.chunk_every([0 | prev], 2, 1) |> Enum.map(&Enum.sum/1)
+    [curr | triangle]
+  end
+
+  defp do_rows(1) do
+    [[1]]
   end
 end
