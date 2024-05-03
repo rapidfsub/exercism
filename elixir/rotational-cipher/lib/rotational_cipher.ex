28ae911ba82b3d082c3a0a588123c27c5931163e
Minsub Kim <rapidfsub@gmail.com>
2024-05-03 19:47:06 +0900

    solve rotational cipher

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -8,5 +8,18 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> to_charlist()
+    |> Enum.map(fn
+      l when l in ?a..?z -> do_rotate(l, shift, ?a)
+      l when l in ?A..?Z -> do_rotate(l, shift, ?A)
+      l -> l
+    end)
+    |> to_string()
+  end
+
+  @alphabet_len Enum.count(?a..?z)
+  defp do_rotate(letter, shift, base) do
+    rem(letter + shift - base, @alphabet_len) + base
   end
 end
