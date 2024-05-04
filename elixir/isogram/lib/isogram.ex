6a892872b8893ad553ab37bec6b031aaaeb09afd
Minsub Kim <rapidfsub@gmail.com>
2024-05-04 11:23:17 +0900

    solve isogram

--- elixir/isogram/lib/isogram.ex
+++ elixir/isogram/lib/isogram.ex
@@ -4,5 +4,11 @@ defmodule Isogram do
   """
   @spec isogram?(String.t()) :: boolean
   def isogram?(sentence) do
+    sentence
+    |> String.replace(~r/[^\p{L}]/u, "")
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.frequencies()
+    |> Enum.all?(&(elem(&1, 1) < 2))
   end
 end
