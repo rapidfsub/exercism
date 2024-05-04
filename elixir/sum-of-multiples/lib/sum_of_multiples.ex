ebfb7e68c512e665a187720e3d2e4063d4d5613b
Minsub Kim <rapidfsub@gmail.com>
2024-05-04 09:14:48 +0900

    solve sum of multiples

--- elixir/sum-of-multiples/lib/sum_of_multiples.ex
+++ elixir/sum-of-multiples/lib/sum_of_multiples.ex
@@ -4,5 +4,10 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
+    factors
+    |> Enum.filter(&(&1 > 0))
+    |> Enum.flat_map(&(&1..(limit - 1)//&1))
+    |> Enum.into(MapSet.new())
+    |> Enum.sum()
   end
 end
