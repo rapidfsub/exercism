c4ca86add3ffa679eb59ddfe47b0206c8be52e3f
Minsub Kim <rapidfsub@gmail.com>
2024-05-06 11:39:26 +0900

    solve ocr numbers

--- elixir/ocr-numbers/lib/ocr_numbers.ex
+++ elixir/ocr-numbers/lib/ocr_numbers.ex
@@ -5,5 +5,144 @@ defmodule OcrNumbers do
   """
   @spec convert([String.t()]) :: {:ok, String.t()} | {:error, String.t()}
   def convert(input) do
+    with {:ok, acc} <-
+           input
+           |> Enum.chunk_every(4)
+           |> Enum.reduce_while({:ok, []}, fn input, {:ok, acc} ->
+             case do_convert(input) do
+               {:ok, result} -> {:cont, {:ok, [result | acc]}}
+               {:error, reason} -> {:halt, {:error, reason}}
+             end
+           end) do
+      {:ok,
+       acc
+       |> Enum.reverse()
+       |> Enum.map(&Enum.reverse/1)
+       |> Enum.map_join(",", &to_string/1)}
+    end
+  end
+
+  defp do_convert([_, _, _, _] = input) do
+    input
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(&Enum.chunk_every(&1, 3))
+    |> Enum.zip()
+    |> Enum.reduce_while({:ok, []}, fn input, {:ok, acc} ->
+      input
+      |> Tuple.to_list()
+      |> Enum.map(&to_string/1)
+      |> convert_digit()
+      |> case do
+        {:ok, result} -> {:cont, {:ok, [result | acc]}}
+        {:error, reason} -> {:halt, {:error, reason}}
+      end
+    end)
+  end
+
+  defp do_convert(_input) do
+    {:error, "invalid line count"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         "| |",
+         "|_|",
+         "   "
+       ]) do
+    {:ok, "0"}
+  end
+
+  defp convert_digit([
+         "   ",
+         "  |",
+         "  |",
+         "   "
+       ]) do
+    {:ok, "1"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         " _|",
+         "|_ ",
+         "   "
+       ]) do
+    {:ok, "2"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         " _|",
+         " _|",
+         "   "
+       ]) do
+    {:ok, "3"}
+  end
+
+  defp convert_digit([
+         "   ",
+         "|_|",
+         "  |",
+         "   "
+       ]) do
+    {:ok, "4"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         "|_ ",
+         " _|",
+         "   "
+       ]) do
+    {:ok, "5"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         "|_ ",
+         "|_|",
+         "   "
+       ]) do
+    {:ok, "6"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         "  |",
+         "  |",
+         "   "
+       ]) do
+    {:ok, "7"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         "|_|",
+         "|_|",
+         "   "
+       ]) do
+    {:ok, "8"}
+  end
+
+  defp convert_digit([
+         " _ ",
+         "|_|",
+         " _|",
+         "   "
+       ]) do
+    {:ok, "9"}
+  end
+
+  defp convert_digit([
+         <<_, _, _>>,
+         <<_, _, _>>,
+         <<_, _, _>>,
+         <<_, _, _>>
+       ]) do
+    {:ok, "?"}
+  end
+
+  defp convert_digit(_input) do
+    {:error, "invalid column count"}
   end
 end
