7d33c67ae71757b06c839dffa3a2c8de04370487
Minsub Kim <rapidfsub@gmail.com>
2024-05-06 09:05:02 +0900

    refactor yacht

--- elixir/yacht/lib/yacht.ex
+++ elixir/yacht/lib/yacht.ex
@@ -17,46 +17,24 @@ defmodule Yacht do
   Calculate the score of 5 dice using the given category's scoring method.
   """
   @spec score(category :: category(), dice :: [integer]) :: integer
-  def score(category, dice) when category in ~w[ones twos threes fours fives sixes]a do
+  def score(category, dice) do
+    Enum.sort(dice) |> do_score(category)
+  end
+
+  defp do_score(dice, category) when category in ~w[ones twos threes fours fives sixes]a do
     die = from_category_to_integer(category)
     die * Enum.count(dice, &(&1 == die))
   end
 
-  def score(:full_house, dice) do
-    dice
-    |> Enum.frequencies()
-    |> Map.values()
-    |> Enum.sort()
-    |> case do
-      [2, 3] -> Enum.sum(dice)
-      _ -> 0
-    end
-  end
-
-  def score(:four_of_a_kind, dice) do
-    case Enum.frequencies(dice) |> Enum.find(&(elem(&1, 1) >= 4)) do
-      {die, _count} -> 4 * die
-      nil -> 0
-    end
-  end
-
-  def score(:little_straight, dice) do
-    case Enum.sort(dice) do
-      [1, 2, 3, 4, 5] -> 30
-      _ -> 0
-    end
-  end
-
-  def score(:big_straight, dice) do
-    case Enum.sort(dice) do
-      [2, 3, 4, 5, 6] -> 30
-      _ -> 0
-    end
-  end
-
-  def score(:choice, dice), do: Enum.sum(dice)
-  def score(:yacht, [d, d, d, d, d]), do: 50
-  def score(:yacht, _dice), do: 0
+  defp do_score([a, a, a, b, b] = dice, :full_house) when a != b, do: Enum.sum(dice)
+  defp do_score([a, a, b, b, b] = dice, :full_house) when a != b, do: Enum.sum(dice)
+  defp do_score([a, a, a, a, _], :four_of_a_kind), do: 4 * a
+  defp do_score([_, a, a, a, a], :four_of_a_kind), do: 4 * a
+  defp do_score([1, 2, 3, 4, 5], :little_straight), do: 30
+  defp do_score([2, 3, 4, 5, 6], :big_straight), do: 30
+  defp do_score([a, a, a, a, a], :yacht), do: 50
+  defp do_score([_, _, _, _, _] = dice, :choice), do: Enum.sum(dice)
+  defp do_score([_, _, _, _, _], _category), do: 0
 
   defp from_category_to_integer(:ones), do: 1
   defp from_category_to_integer(:twos), do: 2
