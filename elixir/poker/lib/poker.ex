2738df1d265422bb1f71a0a1337a09dfc5f93f87
Minsub Kim <rapidfsub@gmail.com>
2024-05-12 12:10:06 +0900

    solve poker

--- elixir/poker/lib/poker.ex
+++ elixir/poker/lib/poker.ex
@@ -29,5 +29,68 @@ defmodule Poker do
   """
   @spec best_hand(list(list(String.t()))) :: list(list(String.t()))
   def best_hand(hands) do
+    [{_, result} | _] = Enum.group_by(hands, &get_score/1) |> Enum.sort_by(&elem(&1, 0), :desc)
+    result
   end
+
+  defp get_score(hand) do
+    hand
+    |> Enum.map(&String.split_at(&1, -1))
+    |> Enum.map(fn {rank, suit} -> {rank_to_integer(rank), suit} end)
+    |> Enum.sort()
+    |> do_get_score()
+  end
+
+  defguardp is_straight(r1, r2, r3, r4, r5)
+            when {r1, r2, r3, r4, r5} == {r1, r1 + 1, r2 + 1, r3 + 1, r4 + 1}
+
+  # straight_flush
+  defp do_get_score([{r1, s}, {r2, s}, {r3, s}, {r4, s}, {r5, s}])
+       when is_straight(r1, r2, r3, r4, r5),
+       do: {8, r5}
+
+  defp do_get_score([{2, s}, {3, s}, {4, s}, {5, s}, {14, s}]), do: {8, 5}
+
+  # four_of_a_kind
+  defp do_get_score([{r2, _}, {r2, _}, {r2, _}, {r2, _}, {r1, _}]), do: {7, {r2, r1}}
+  defp do_get_score([{r1, _}, {r2, _}, {r2, _}, {r2, _}, {r2, _}]), do: {7, {r2, r1}}
+
+  # full_house
+  defp do_get_score([{r1, _}, {r1, _}, {r1, _}, {r2, _}, {r2, _}]), do: {6, {r1, r2}}
+  defp do_get_score([{r1, _}, {r1, _}, {r2, _}, {r2, _}, {r2, _}]), do: {6, {r2, r1}}
+
+  # flush
+  defp do_get_score([{r1, s}, {r2, s}, {r3, s}, {r4, s}, {r5, s}]), do: {5, {r5, r4, r3, r2, r1}}
+
+  # straight
+  defp do_get_score([{r1, _s1}, {r2, _s2}, {r3, _s3}, {r4, _s4}, {r5, _s5}])
+       when is_straight(r1, r2, r3, r4, r5),
+       do: {4, r5}
+
+  defp do_get_score([{2, _s1}, {3, _s2}, {4, _s3}, {5, _s4}, {14, _s5}]), do: {4, 5}
+
+  # three_of_a_kind
+  defp do_get_score([{r3, _}, {r3, _}, {r3, _}, {r1, _}, {r2, _}]), do: {3, {r3, r2, r1}}
+  defp do_get_score([{r1, _}, {r3, _}, {r3, _}, {r3, _}, {r2, _}]), do: {3, {r3, r2, r1}}
+  defp do_get_score([{r1, _}, {r2, _}, {r3, _}, {r3, _}, {r3, _}]), do: {3, {r3, r2, r1}}
+
+  # two_pair
+  defp do_get_score([{r2, _}, {r2, _}, {r3, _}, {r3, _}, {r1, _}]), do: {2, {r3, r2, r1}}
+  defp do_get_score([{r2, _}, {r2, _}, {r1, _}, {r3, _}, {r3, _}]), do: {2, {r3, r2, r1}}
+  defp do_get_score([{r1, _}, {r2, _}, {r2, _}, {r3, _}, {r3, _}]), do: {2, {r3, r2, r1}}
+
+  # one_pair
+  defp do_get_score([{r4, _}, {r4, _}, {r1, _}, {r2, _}, {r3, _}]), do: {1, {r4, r3, r2, r1}}
+  defp do_get_score([{r1, _}, {r4, _}, {r4, _}, {r2, _}, {r3, _}]), do: {1, {r4, r3, r2, r1}}
+  defp do_get_score([{r1, _}, {r2, _}, {r4, _}, {r4, _}, {r3, _}]), do: {1, {r4, r3, r2, r1}}
+  defp do_get_score([{r1, _}, {r2, _}, {r3, _}, {r4, _}, {r4, _}]), do: {1, {r4, r3, r2, r1}}
+
+  # high_card
+  defp do_get_score([{r1, _}, {r2, _}, {r3, _}, {r4, _}, {r5, _}]), do: {0, {r5, r4, r3, r2, r1}}
+
+  defp rank_to_integer("J"), do: 11
+  defp rank_to_integer("Q"), do: 12
+  defp rank_to_integer("K"), do: 13
+  defp rank_to_integer("A"), do: 14
+  defp rank_to_integer(rank), do: String.to_integer(rank)
 end
--- elixir/poker/test/poker_test.exs
+++ elixir/poker/test/poker_test.exs
@@ -30,7 +30,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [high_of_jack])
   end
 
-  @tag :pending
+  # @tag :pending
   test "highest card out of all hands wins" do
     high_of_8 = ~w(4D 5S 6S 8D 3C)
     high_of_10 = ~w(2S 4C 7S 9H 10H)
@@ -39,7 +39,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [high_of_jack])
   end
 
-  @tag :pending
+  # @tag :pending
   test "a tie has multiple winners" do
     high_of_8 = ~w(4D 5S 6S 8D 3C)
     high_of_10 = ~w(2S 4C 7S 9H 10H)
@@ -51,7 +51,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [high_of_jack, another_high_of_jack])
   end
 
-  @tag :pending
+  # @tag :pending
   test "multiple hands with the same high cards, tie compares next highest ranked, down to last card" do
     high_of_8_low_of_3 = ~w(3S 5H 6S 8D 7H)
     high_of_8_low_of_2 = ~w(2S 5D 6D 8C 7S)
@@ -59,7 +59,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [high_of_8_low_of_3])
   end
 
-  @tag :pending
+  # @tag :pending
   test "winning high card hand also has the lowest card" do
     high_of_8_low_of_5_and_3 = ~w(2S 5H 6S 8D 7H)
     high_of_8_low_of_4_and_2 = ~w(3S 4D 6D 8C 7S)
@@ -67,7 +67,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [high_of_8_low_of_5_and_3])
   end
 
-  @tag :pending
+  # @tag :pending
   test "one pair beats high card" do
     high_of_king = ~w(4S 5H 6C 8D KH)
     pair_of_4 = ~w(2S 4H 6S 4D JH)
@@ -75,7 +75,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [pair_of_4])
   end
 
-  @tag :pending
+  # @tag :pending
   test "highest pair wins" do
     pair_of_2 = ~w(4S 2H 6S 2D JH)
     pair_of_4 = ~w(2S 4H 6C 4D JD)
@@ -83,7 +83,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [pair_of_4])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have the same pair, high card wins" do
     pair_of_4_low_of_j_and_3 = ~w(4H 4S AH JC 3D)
     pair_of_4_low_of_6_and_5 = ~w(4C 4D AS 5D 6C)
@@ -91,7 +91,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [pair_of_4_low_of_j_and_3])
   end
 
-  @tag :pending
+  # @tag :pending
   test "two pairs beats one pair" do
     pair_of_8 = ~w(2S 8H 6S 8D JH)
     fives_and_fours = ~w(4S 5H 4C 8C 5C)
@@ -99,7 +99,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [fives_and_fours])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have two pairs, highest ranked pair wins" do
     eights_and_twos = ~w(2S 8H 2D 8D 3H)
     fives_and_fours = ~w(4S 5H 4C 8S 5D)
@@ -107,7 +107,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [eights_and_twos])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have two pairs, with the same highest ranked pair, tie goes to low pair" do
     queens_and_twos = ~w(2S QS 2C QD JH)
     queens_and_jacks = ~w(JD QH JS 8D QC)
@@ -115,7 +115,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [queens_and_jacks])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have two identically ranked pairs, tie goes to remaining card (kicker)" do
     queens_jacks_and_8 = ~w(JD QH JS 8D QC)
     queens_jacks_and_2 = ~w(JS QS JC 2D QD)
@@ -123,7 +123,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [queens_jacks_and_8])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have two pairs that add to the same value, win goes to highest pair" do
     sixes_threes_and_ace = ~w(6S 6H 3S 3H AS)
     sevens_twos_and_ace = ~w(7H 7S 2H 2S AC)
@@ -131,7 +131,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [sevens_twos_and_ace])
   end
 
-  @tag :pending
+  # @tag :pending
   test "two pairs first ranked by largest pair" do
     fives_fours_and_two = ~w(5C 2S 5S 4H 4C)
     sixes_twos_and_seven = ~w(6S 2S 6H 7C 2C)
@@ -139,7 +139,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [sixes_twos_and_seven])
   end
 
-  @tag :pending
+  # @tag :pending
   test "three of a kind beats two pair" do
     eights_and_twos = ~w(2S 8H 2H 8D JH)
     three_fours = ~w(4S 5H 4C 8S 4H)
@@ -147,7 +147,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [three_fours])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have three of a kind, tie goes to highest ranked triplet" do
     three_twos = ~w(2S 2H 2C 8D JH)
     three_aces = ~w(4S AH AS 8C AD)
@@ -155,7 +155,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [three_aces])
   end
 
-  @tag :pending
+  # @tag :pending
   test "with multiple decks, two players can have same three of a kind, ties go to highest remaining cards" do
     three_aces_7_high = ~w(5S AH AS 7C AD)
     three_aces_8_high = ~w(4S AH AS 8C AD)
@@ -163,7 +163,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [three_aces_8_high])
   end
 
-  @tag :pending
+  # @tag :pending
   test "a straight beats three of a kind" do
     three_fours = ~w(4S 5H 4C 8D 4H)
     straight = ~w(3S 4D 2S 6D 5C)
@@ -171,7 +171,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight])
   end
 
-  @tag :pending
+  # @tag :pending
   test "aces can end a straight (10 J Q K A)" do
     three_fours = ~w(4S 5H 4C 8D 4H)
     straight_to_ace = ~w(10D JH QS KD AC)
@@ -179,7 +179,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_to_ace])
   end
 
-  @tag :pending
+  # @tag :pending
   test "aces can start a straight (A 2 3 4 5)" do
     three_fours = ~w(4S 5H 4C 8D 4H)
     straight_to_5 = ~w(4D AH 3S 2D 5C)
@@ -187,7 +187,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_to_5])
   end
 
-  @tag :pending
+  # @tag :pending
   test "aces cannot be in the middle of a straight (Q K A 2 3)" do
     pair = ~w(2C 3D 7H 5H 2S)
     not_a_straight = ~w(QS KH AC 2D 3S)
@@ -195,7 +195,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [pair])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands with a straight, tie goes to highest ranked card" do
     straight_to_8 = ~w(4S 6C 7S 8D 5H)
     straight_to_9 = ~w(5S 7H 8S 9D 6H)
@@ -203,7 +203,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_to_9])
   end
 
-  @tag :pending
+  # @tag :pending
   test "even though an ace is usually high, a 5-high straight is the lowest-scoring straight" do
     straight_to_6 = ~w(2H 3C 4D 5D 6H)
     straight_to_5 = ~w(4S AH 3S 2D 5H)
@@ -211,7 +211,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_to_6])
   end
 
-  @tag :pending
+  # @tag :pending
   test "flush beats a straight" do
     straight_to_8 = ~w(4C 6H 7D 8D 5H)
     flush_to_7 = ~w(2S 4S 5S 6S 7S)
@@ -219,7 +219,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [flush_to_7])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have a flush, tie goes to high card, down to the last one if necessary" do
     flush_to_9 = ~w(2H 7H 8H 9H 6H)
     flush_to_8 = ~w(3S 5S 6S 7S 8S)
@@ -227,7 +227,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [flush_to_9])
   end
 
-  @tag :pending
+  # @tag :pending
   test "full house beats a flush" do
     flush_to_8 = ~w(3H 6H 7H 8H 5H)
     full = ~w(4S 5H 4C 5D 4H)
@@ -235,7 +235,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [full])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have a full house, tie goes to highest-ranked triplet" do
     full_of_4_by_9 = ~w(4H 4S 4D 9S 9D)
     full_of_5_by_8 = ~w(5H 5S 5D 8S 8D)
@@ -243,7 +243,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [full_of_5_by_8])
   end
 
-  @tag :pending
+  # @tag :pending
   test "with multiple decks, both hands have a full house with the same triplet, tie goes to the pair" do
     full_of_5_by_9 = ~w(5H 5S 5D 9S 9D)
     full_of_5_by_8 = ~w(5H 5S 5D 8S 8D)
@@ -251,7 +251,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [full_of_5_by_9])
   end
 
-  @tag :pending
+  # @tag :pending
   test "four of a kind beats a full house" do
     full = ~w(4S 5H 4D 5D 4H)
     four_3s = ~w(3S 3H 2S 3D 3C)
@@ -259,7 +259,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [four_3s])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have four of a kind, tie goes to high quad" do
     four_2s = ~w(2S 2H 2C 8D 2D)
     four_5s = ~w(4S 5H 5S 5D 5C)
@@ -267,7 +267,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [four_5s])
   end
 
-  @tag :pending
+  # @tag :pending
   test "with multiple decks, both hands with identical four of a kind, tie determined by kicker" do
     four_3s_and_2 = ~w(3S 3H 2S 3D 3C)
     four_3s_and_4 = ~w(3S 3H 4S 3D 3C)
@@ -275,7 +275,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [four_3s_and_4])
   end
 
-  @tag :pending
+  # @tag :pending
   test "straight flush beats four of a kind" do
     four_5s = ~w(4S 5H 5S 5D 5C)
     straight_flush_to_10 = ~w(7S 8S 9S 6S 10S)
@@ -283,7 +283,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_flush_to_10])
   end
 
-  @tag :pending
+  # @tag :pending
   test "aces can end a straight flush (10 J Q K A)" do
     four_aces = ~w(KC AH AS AD AC)
     straight_flush_to_ace = ~w(10C JC QC KC AC)
@@ -291,7 +291,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_flush_to_ace])
   end
 
-  @tag :pending
+  # @tag :pending
   test "aces can start a straight flush (A 2 3 4 5)" do
     four_aces = ~w(KS AH AS AD AC)
     straight_flush_to_5 = ~w(4H AH 3H 2H 5H)
@@ -299,7 +299,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_flush_to_5])
   end
 
-  @tag :pending
+  # @tag :pending
   test "aces cannot be in the middle of a straight flush (Q K A 2 3)" do
     nothing = ~w(2C AC QC 10C KC)
     not_a_straight_flush = ~w(QH KH AH 2H 3H)
@@ -307,7 +307,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [nothing])
   end
 
-  @tag :pending
+  # @tag :pending
   test "both hands have a straight flush, tie goes to highest-ranked card" do
     straight_flush_to_8 = ~w(4H 6H 7H 8H 5H)
     straight_flush_to_9 = ~w(5S 7S 8S 9S 6S)
@@ -315,7 +315,7 @@ defmodule PokerTest do
     assert_poker(winning_hands, [straight_flush_to_9])
   end
 
-  @tag :pending
+  # @tag :pending
   test "even though an ace is usually high, a 5-high straight flush is the lowest-scoring straight flush" do
     straight_flush_to_6 = ~w(2H 3H 4H 5H 6H)
     straight_flush_to_5 = ~w(4D AD 3D 2D 5D)
