07da17ec14a483e5a2828fa09db6ec3360ae4931
Minsub Kim <rapidfsub@gmail.com>
2024-05-15 14:31:02 +0900

    solve crypto square

--- elixir/crypto-square/lib/crypto_square.ex
+++ elixir/crypto-square/lib/crypto_square.ex
@@ -8,5 +8,27 @@ defmodule CryptoSquare do
   """
   @spec encode(String.t()) :: String.t()
   def encode(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\p{L}\p{N}]/i, "")
+    |> do_encode()
+  end
+
+  defp do_encode("") do
+    ""
+  end
+
+  defp do_encode(str) do
+    len = String.length(str)
+    height = :math.sqrt(len) |> ceil()
+    width = ceil(len / height)
+
+    str
+    |> String.pad_trailing(width * height, " ")
+    |> String.graphemes()
+    |> Enum.chunk_every(height)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.join(" ")
   end
 end
