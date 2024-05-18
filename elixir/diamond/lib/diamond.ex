f80f90033b27bf143f14687c355ab790bc070d19
Minsub Kim <rapidfsub@gmail.com>
2024-05-18 14:17:53 +0900

    solve diamond

--- elixir/diamond/lib/diamond.ex
+++ elixir/diamond/lib/diamond.ex
@@ -4,6 +4,21 @@ defmodule Diamond do
   with the supplied letter at the widest point.
   """
   @spec build_shape(char) :: String.t()
-  def build_shape(letter) do
+  def build_shape(letter) when letter in ?A..?Z do
+    len = letter - ?A + 1
+
+    triangle =
+      ?A..?Z
+      |> Enum.take(len)
+      |> Enum.with_index(fn l, i ->
+        pad = List.duplicate(?\s, len - i - 1)
+        {left, [pivot]} = [l | List.duplicate(?\s, i)] |> Enum.split(-1)
+        [pad, left, pivot, Enum.reverse(left), pad, ?\n]
+      end)
+
+    triangle
+    |> Enum.drop(-1)
+    |> Enum.concat(Enum.reverse(triangle))
+    |> to_string()
   end
 end
