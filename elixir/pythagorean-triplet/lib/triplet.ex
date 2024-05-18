abbaea78d1d96e8055240c90652e32f2b8a00898
Minsub Kim <rapidfsub@gmail.com>
2024-05-18 16:05:16 +0900

    solve triplet

--- elixir/pythagorean-triplet/lib/triplet.ex
+++ elixir/pythagorean-triplet/lib/triplet.ex
@@ -3,14 +3,16 @@ defmodule Triplet do
   Calculates sum of a given triplet of integers.
   """
   @spec sum([non_neg_integer]) :: non_neg_integer
-  def sum(triplet) do
+  def sum([_, _, _] = t) do
+    Enum.sum(t)
   end
 
   @doc """
   Calculates product of a given triplet of integers.
   """
   @spec product([non_neg_integer]) :: non_neg_integer
-  def product(triplet) do
+  def product([_, _, _] = t) do
+    Enum.product(t)
   end
 
   @doc """
@@ -18,6 +20,7 @@ defmodule Triplet do
   """
   @spec pythagorean?([non_neg_integer]) :: boolean
   def pythagorean?([a, b, c]) do
+    a ** 2 + b ** 2 == c ** 2
   end
 
   @doc """
@@ -25,5 +28,17 @@ defmodule Triplet do
   """
   @spec generate(non_neg_integer) :: [list(non_neg_integer)]
   def generate(sum) do
+    cs = div(sum, 3)..div(sum, 2)
+    count = ceil(Enum.count(cs) / System.schedulers_online())
+
+    Stream.chunk_every(cs, count, count, [])
+    |> Stream.map(fn cs ->
+      for c <- cs, b <- ceil((sum - c) / 2)..(sum - c - 1) do
+        [sum - c - b, b, c]
+      end
+    end)
+    |> Task.async_stream(&Enum.filter(&1, fn x -> pythagorean?(x) end))
+    |> Stream.flat_map(&elem(&1, 1))
+    |> Enum.sort()
   end
 end
