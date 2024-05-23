58fc5f76c4a491e90d541db394b16b2209fc9435
Minsub Kim <rapidfsub@gmail.com>
2024-05-23 14:31:15 +0900

    solve knapsack

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -5,5 +5,19 @@ defmodule Knapsack do
   @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
           integer
   def maximum_value(items, maximum_weight) do
+    do_maximum_value(items, maximum_weight, 0, 0)
   end
+
+  defp do_maximum_value([%{weight: dw, value: dv} | items], w_max, w, v) do
+    case {do_maximum_value(items, w_max, w, v), do_maximum_value(items, w_max, w + dw, v + dv)} do
+      {nil, nil} when w_max < w -> nil
+      {nil, nil} -> v
+      {nil, result} -> result
+      {result, nil} -> result
+      {lhs, rhs} -> max(lhs, rhs)
+    end
+  end
+
+  defp do_maximum_value([], w_max, w, _v) when w_max < w, do: nil
+  defp do_maximum_value([], _w_max, _w, v), do: v
 end
