1e64a6ad68571c232eb97e66ded953bbd46e782d
Minsub Kim <rapidfsub@gmail.com>
2024-05-23 14:44:09 +0900

    refactor knapsack

--- elixir/knapsack/lib/knapsack.ex
+++ elixir/knapsack/lib/knapsack.ex
@@ -2,22 +2,11 @@ defmodule Knapsack do
   @doc """
   Return the maximum value that a knapsack can carry.
   """
-  @spec maximum_value(items :: [%{value: integer, weight: integer}], maximum_weight :: integer) ::
-          integer
-  def maximum_value(items, maximum_weight) do
-    do_maximum_value(items, maximum_weight, 0, 0)
+  @spec maximum_value(items :: [%{value: integer, weight: integer}], w_max :: integer) :: integer
+  def maximum_value([%{weight: w, value: v} | items], w_max) when w <= w_max do
+    max(v + maximum_value(items, w_max - w), maximum_value(items, w_max))
   end
 
-  defp do_maximum_value([%{weight: dw, value: dv} | items], w_max, w, v) do
-    case {do_maximum_value(items, w_max, w, v), do_maximum_value(items, w_max, w + dw, v + dv)} do
-      {nil, nil} when w_max < w -> nil
-      {nil, nil} -> v
-      {nil, result} -> result
-      {result, nil} -> result
-      {lhs, rhs} -> max(lhs, rhs)
-    end
-  end
-
-  defp do_maximum_value([], w_max, w, _v) when w_max < w, do: nil
-  defp do_maximum_value([], _w_max, _w, v), do: v
+  def maximum_value([_item | items], w_max), do: maximum_value(items, w_max)
+  def maximum_value([], _w_max), do: 0
 end
