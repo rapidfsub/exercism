4fedddba733b6347c542e9e074cbf4e29aefcbe5
Minsub Kim <rapidfsub@gmail.com>
2024-06-04 10:46:02 +0900

    solve pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,17 @@
 module Pangram (isPangram) where
 
+import Data.Char as Char (isAlpha, isAscii, toLower)
+import Data.Function ((&))
+import Data.Set as Set (fromList, size)
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text =
+  text
+    & filter Char.isAscii
+    & filter Char.isAlpha
+    & map Char.toLower
+    & Set.fromList
+    & Set.size
+    & (==) alphabetLen
+  where
+    alphabetLen = length ['a', 'b' .. 'z']
