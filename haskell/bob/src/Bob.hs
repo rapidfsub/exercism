ed29ab8be73da774a1c1bbdf783e528d505a40c6
Minsub Kim <rapidfsub@gmail.com>
2024-06-06 11:30:13 +0900

    solve bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bob (responseFor) where
 
+import Data.Text as T
+  ( empty,
+    isSuffixOf,
+    pack,
+    strip,
+    toLower,
+    toUpper,
+  )
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | isShouting && isAsking = "Calm down, I know what I'm doing!"
+  | isShouting = "Whoa, chill out!"
+  | isAsking = "Sure."
+  | isSilent = "Fine. Be that way!"
+  | otherwise = "Whatever."
+  where
+    ys = T.strip $ T.pack $ xs
+    isShouting = ys == T.toUpper ys && ys /= T.toLower ys
+    isAsking = T.isSuffixOf "?" ys
+    isSilent = ys == T.empty
