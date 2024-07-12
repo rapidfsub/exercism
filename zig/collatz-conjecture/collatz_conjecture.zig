9f057ebb72c9223069bf6f164c1b0885cd15834d
Minsub Kim <rapidfsub@gmail.com>
2024-07-12 20:35:40 +0900

    solve collatz conjecture

--- zig/collatz-conjecture/collatz_conjecture.zig
+++ zig/collatz-conjecture/collatz_conjecture.zig
@@ -1,6 +1,19 @@
 // Please implement the `ComputationError.IllegalArgument` error.
 
-pub fn steps(number: usize) anyerror!usize {
-    _ = number;
-    @compileError("please implement the steps function");
+pub const ComputationError = error{IllegalArgument};
+
+pub fn steps(number: usize) ComputationError!usize {
+    if (number > 1) {
+        if (number % 2 == 0) {
+            return 1 + try steps(number / 2);
+        } else {
+            return 1 + try steps(3 * number + 1);
+        }
+    } else {
+        if (number > 0) {
+            return 0;
+        } else {
+            return ComputationError.IllegalArgument;
+        }
+    }
 }
