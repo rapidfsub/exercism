c84102e08b4fdca960e7cbacc7fd4ce30603f00f
Minsub Kim <rapidfsub@gmail.com>
2024-07-14 17:39:06 +0900

    solve resistor color

--- zig/resistor-color/resistor_color.zig
+++ zig/resistor-color/resistor_color.zig
@@ -1,7 +1,31 @@
+pub const ColorBand = enum(usize) {
+    black = 0,
+    brown = 1,
+    red = 2,
+    orange = 3,
+    yellow = 4,
+    green = 5,
+    blue = 6,
+    violet = 7,
+    grey = 8,
+    white = 9,
+};
+
 pub fn colorCode(color: ColorBand) usize {
-    @compileError("determine the value of a colorband on a resistor");
+    return @intFromEnum(color);
 }
 
 pub fn colors() []const ColorBand {
-    @compileError("refer to a collection of all resistor colorbands");
+    return &[_]ColorBand{
+        .black,
+        .brown,
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .blue,
+        .violet,
+        .grey,
+        .white,
+    };
 }
