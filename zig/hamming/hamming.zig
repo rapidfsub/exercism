b6c51b695682ded483fad87468440b9ab303ac96
Minsub Kim <rapidfsub@gmail.com>
2024-07-14 16:01:38 +0900

    solve hamming

--- zig/hamming/hamming.zig
+++ zig/hamming/hamming.zig
@@ -1,5 +1,22 @@
+const std = @import("std");
+
+pub const DnaError = error{
+    EmptyDnaStrands,
+    UnequalDnaStrands,
+};
+
 pub fn compute(first: []const u8, second: []const u8) DnaError!usize {
-    _ = first;
-    _ = second;
-    @compileError("please implement the compute function");
+    if (first.len < 1 or second.len < 1) {
+        return DnaError.EmptyDnaStrands;
+    } else if (first.len != second.len) {
+        return DnaError.UnequalDnaStrands;
+    } else {
+        var result: usize = 0;
+        for (first, second) |lhs, rhs| {
+            if (lhs != rhs) {
+                result += 1;
+            }
+        }
+        return result;
+    }
 }
