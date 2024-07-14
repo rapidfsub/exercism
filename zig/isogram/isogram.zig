a78938b6d21ff508f602c3172c52f2974c786c1b
Minsub Kim <rapidfsub@gmail.com>
2024-07-14 15:55:49 +0900

    solve isogram

--- zig/isogram/isogram.zig
+++ zig/isogram/isogram.zig
@@ -1,4 +1,17 @@
+const std = @import("std");
+
 pub fn isIsogram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isIsogram function");
+    var tally: u32 = 0;
+    for (str) |letter| {
+        if (std.ascii.isAlphabetic(letter)) {
+            const exp = std.ascii.toLower(letter) - 'a';
+            const bit = std.math.pow(u32, 2, exp);
+            if (tally & bit > 0) {
+                return false;
+            } else {
+                tally |= bit;
+            }
+        }
+    }
+    return true;
 }
