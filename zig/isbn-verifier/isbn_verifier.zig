24dec3d2175f4e22f3318027dccb67979f61ccae
Minsub Kim <rapidfsub@gmail.com>
2024-07-14 17:19:41 +0900

    solve isbn verifier

--- zig/isbn-verifier/isbn_verifier.zig
+++ zig/isbn-verifier/isbn_verifier.zig
@@ -1,4 +1,16 @@
 pub fn isValidIsbn10(s: []const u8) bool {
-    _ = s;
-    @compileError("please implement the isValidIsbn10 function");
+    var sum: i64 = 0;
+    var c: i64 = 10;
+    for (s) |letter| {
+        if (letter >= '0' and letter <= '9') {
+            sum += (letter - '0') * c;
+            c -= 1;
+        } else if (c == 1 and letter == 'X') {
+            sum += 10;
+            c -= 1;
+        } else if (letter != ' ' and letter != '-') {
+            return false;
+        }
+    }
+    return c == 0 and @rem(sum, 11) == 0;
 }
