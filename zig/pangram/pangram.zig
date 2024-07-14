9f79e88c51a66c7b17ed463493a93fc79711e0e6
Minsub Kim <rapidfsub@gmail.com>
2024-07-14 13:25:11 +0900

    solve pangram

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -1,4 +1,12 @@
+const std = @import("std");
+
 pub fn isPangram(str: []const u8) bool {
-    _ = str;
-    @compileError("please implement the isPangram function");
+    var tally: u32 = 0;
+    for (str) |letter| {
+        if (std.ascii.isAlphabetic(letter)) {
+            const exp = std.ascii.toLower(letter) - 'a';
+            tally |= std.math.pow(u32, 2, exp);
+        }
+    }
+    return tally == 0x3_FF_FF_FF;
 }
