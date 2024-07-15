4871706a17ac7779933299f012043bebd67b9a53
Minsub Kim <rapidfsub@gmail.com>
2024-07-15 19:28:44 +0900

    solve nucleotide count

--- zig/nucleotide-count/nucleotide_count.zig
+++ zig/nucleotide-count/nucleotide_count.zig
@@ -5,8 +5,32 @@ pub const Counts = struct {
     c: u32,
     g: u32,
     t: u32,
+
+    pub fn init() Counts {
+        return Counts{ .a = 0, .c = 0, .g = 0, .t = 0 };
+    }
 };
 
 pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
-    _ = s;
+    var counts = Counts.init();
+    for (s) |letter| {
+        switch (letter) {
+            'A' => {
+                counts.a += 1;
+            },
+            'C' => {
+                counts.c += 1;
+            },
+            'G' => {
+                counts.g += 1;
+            },
+            'T' => {
+                counts.t += 1;
+            },
+            else => {
+                return NucleotideError.Invalid;
+            },
+        }
+    }
+    return counts;
 }
