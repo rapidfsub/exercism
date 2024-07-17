875d22adc02d4b08dbe70ef99f5b5422db624a91
Minsub Kim <rapidfsub@gmail.com>
2024-07-18 06:15:17 +0900

    solve reverse string

--- zig/reverse-string/reverse_string.zig
+++ zig/reverse-string/reverse_string.zig
@@ -1,6 +1,11 @@
+const std = @import("std");
+
 /// Writes a reversed copy of `s` to `buffer`.
 pub fn reverse(buffer: []u8, s: []const u8) []u8 {
-    _ = buffer;
-    _ = s;
-    @compileError("please implement the reverse function");
+    var iter = std.mem.reverseIterator(s);
+    var i: usize = 0;
+    while (iter.next()) |letter| : (i += 1) {
+        buffer[i] = letter;
+    }
+    return buffer;
 }
