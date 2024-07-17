923f711dd3200d25dccfb7ed92da9a5ca0557715
Minsub Kim <rapidfsub@gmail.com>
2024-07-18 06:03:50 +0900

    solve perfect numbers

--- zig/perfect-numbers/perfect_numbers.zig
+++ zig/perfect-numbers/perfect_numbers.zig
@@ -6,6 +6,18 @@ pub const Classification = enum {
 
 /// Asserts that `n` is nonzero.
 pub fn classify(n: u64) Classification {
-    _ = n;
-    @compileError("please implement the classify function");
+    var sum: u64 = 0;
+    for (1..n) |k| {
+        if (@rem(n, k) == 0) {
+            sum += k;
+        }
+    }
+
+    if (n < sum) {
+        return .abundant;
+    } else if (n == sum) {
+        return .perfect;
+    } else {
+        return .deficient;
+    }
 }
