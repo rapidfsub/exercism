27cded4f2b706f1a190ba87e4eb158cce4fc0136
Minsub Kim <rapidfsub@gmail.com>
2024-07-17 16:20:26 +0900

    refactor bob

--- zig/bob/bob.zig
+++ zig/bob/bob.zig
@@ -1,39 +1,47 @@
 const std = @import("std");
 
 pub fn response(s: []const u8) []const u8 {
-    var hasUpper = false;
-    var hasLower = false;
-    var isAsking = false;
-    var isSilent = true;
-
-    for (s) |letter| {
-        if (!std.ascii.isWhitespace(letter)) {
-            if (std.ascii.isUpper(letter)) {
-                hasUpper = true;
-            } else if (std.ascii.isLower(letter)) {
-                hasLower = true;
-            }
-
-            isAsking = letter == '?';
-            if (isSilent) {
-                isSilent = false;
-            }
-        }
-    }
-
-    if (isSilent) {
+    if (isSilent(s)) {
         return "Fine. Be that way!";
-    } else if (hasUpper and !hasLower) {
-        if (isAsking) {
+    } else if (isYelling(s)) {
+        if (isAsking(s)) {
             return "Calm down, I know what I'm doing!";
         } else {
             return "Whoa, chill out!";
         }
     } else {
-        if (isAsking) {
+        if (isAsking(s)) {
             return "Sure.";
         } else {
             return "Whatever.";
         }
     }
+}
+
+fn isYelling(s: []const u8) bool {
+    var hasUpper = false;
+    return for (s) |letter| {
+        if (std.ascii.isUpper(letter)) {
+            hasUpper = true;
+        } else if (std.ascii.isLower(letter)) {
+            break false;
+        }
+    } else hasUpper;
+}
+
+fn isAsking(s: []const u8) bool {
+    var result = false;
+    return for (s) |letter| {
+        if (!std.ascii.isWhitespace(letter)) {
+            result = letter == '?';
+        }
+    } else result;
+}
+
+fn isSilent(s: []const u8) bool {
+    return for (s) |letter| {
+        if (!std.ascii.isWhitespace(letter)) {
+            break false;
+        }
+    } else true;
 }
