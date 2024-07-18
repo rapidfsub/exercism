40dc001e835eb8d1b5bd8949180cd01c4309b721
Minsub Kim <rapidfsub@gmail.com>
2024-07-18 14:22:42 +0900

    solve rna transcription

--- zig/rna-transcription/rna_transcription.zig
+++ zig/rna-transcription/rna_transcription.zig
@@ -1,7 +1,15 @@
-// Import the appropriate standard library and modules
+const mem = @import("std").mem;
 
 pub fn toRna(allocator: mem.Allocator, dna: []const u8) mem.Allocator.Error![]const u8 {
-    _ = allocator;
-    _ = dna;
-    @compileError("please implement the toRna function");
+    var rna = try allocator.alloc(u8, dna.len);
+    for (dna, 0..) |c, i| {
+        switch (c) {
+            'G' => rna[i] = 'C',
+            'C' => rna[i] = 'G',
+            'T' => rna[i] = 'A',
+            'A' => rna[i] = 'U',
+            else => unreachable,
+        }
+    }
+    return rna;
 }
