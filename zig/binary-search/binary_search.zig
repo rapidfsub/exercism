3d16e02faf883713eb003f43d7857711d2e79df1
Minsub Kim <rapidfsub@gmail.com>
2024-07-22 10:57:41 +0900

    solve binary search

--- zig/binary-search/binary_search.zig
+++ zig/binary-search/binary_search.zig
@@ -1,7 +1,17 @@
 // Take a look at the tests, you might have to change the function arguments
 
-pub fn binarySearch(target: usize, items: []const usize) ?usize {
-    _ = target;
-    _ = items;
-    @compileError("please implement the binarySearch function");
+pub fn binarySearch(T: type, target: T, items: []const T) ?usize {
+    var i: usize = 0;
+    var j = items.len;
+    return while (i < j) {
+        const p = (i + j) / 2;
+        const pivot = items[p];
+        if (pivot < target) {
+            i = p + 1;
+        } else if (target < pivot) {
+            j = p;
+        } else {
+            break p;
+        }
+    } else null;
 }
