38ff90e6e001134cb80fc530bd5e7dc6be993e55
Minsub Kim <rapidfsub@gmail.com>
2024-07-22 15:32:08 +0900

    solve matching brackets

--- zig/matching-brackets/matching_brackets.zig
+++ zig/matching-brackets/matching_brackets.zig
@@ -1,8 +1,26 @@
 const std = @import("std");
 const mem = std.mem;
+const ArrayList = std.ArrayList;
 
 pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
-    _ = allocator;
-    _ = s;
-    @compileError("please implement the isBalanced function");
+    var stack = ArrayList(u8).init(allocator);
+    defer stack.deinit();
+    for (s) |letter| {
+        switch (letter) {
+            '(', '{', '[' => try stack.append(letter),
+            inline ')', '}', ']' => |close| {
+                const open = switch (close) {
+                    ')' => '(',
+                    '}' => '{',
+                    ']' => '[',
+                    else => unreachable,
+                };
+                if (stack.popOrNull() != open) {
+                    return false;
+                }
+            },
+            else => {},
+        }
+    }
+    return stack.items.len == 0;
 }
