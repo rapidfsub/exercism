2039042354a9b695a02e2cf0482aa75ccbc1543b
Minsub Kim <rapidfsub@gmail.com>
2024-07-23 17:01:21 +0900

    solve acronym

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
 const mem = std.mem;
+const ascii = std.ascii;
+const ArrayList = std.ArrayList;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = words;
-    @compileError("please implement the abbreviate function");
+    var list = ArrayList(u8).init(allocator);
+    defer list.deinit();
+
+    var ws = mem.tokenizeAny(u8, words, " _-");
+    while (ws.next()) |w| {
+        try list.append(ascii.toUpper(w[0]));
+    }
+    return list.toOwnedSlice();
 }
