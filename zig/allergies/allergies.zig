bda6f205e991bf0d0cda949f0b65bb0dd0d0de5a
Minsub Kim <rapidfsub@gmail.com>
2024-07-23 17:35:57 +0900

    solve allergies

--- zig/allergies/allergies.zig
+++ zig/allergies/allergies.zig
@@ -1,24 +1,29 @@
 const std = @import("std");
 const EnumSet = std.EnumSet;
 
-pub const Allergen = enum {
-    eggs,
-    peanuts,
-    shellfish,
-    strawberries,
-    tomatoes,
-    chocolate,
-    pollen,
-    cats,
+pub const Allergen = enum(u32) {
+    eggs = 1,
+    peanuts = 2,
+    shellfish = 4,
+    strawberries = 8,
+    tomatoes = 16,
+    chocolate = 32,
+    pollen = 64,
+    cats = 128,
 };
 
 pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
-    _ = score;
-    _ = allergen;
-    @compileError("please implement the isAllergicTo function");
+    return score & @intFromEnum(allergen) > 0;
 }
 
+const allergens = std.enums.values(Allergen);
 pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
-    _ = score;
-    @compileError("please implement the initAllergenSet function");
+    var result = EnumSet(Allergen).init(.{});
+    inline for (allergens) |allergen| {
+        const bit = @intFromEnum(allergen);
+        if (score & bit > 0) {
+            result.insert(allergen);
+        }
+    }
+    return result;
 }
