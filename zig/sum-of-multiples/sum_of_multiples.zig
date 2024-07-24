913fab483603b75273ddbf99b4cec8187800aa49
Minsub Kim <rapidfsub@gmail.com>
2024-07-24 21:58:43 +0900

    refactor sum of multiples using ArrayList

--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -1,23 +1,28 @@
 const std = @import("std");
 const mem = std.mem;
-const DynamicBitSet = std.bit_set.DynamicBitSet;
+const sort = std.sort;
+const ArrayList = std.ArrayList;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    var multiples = try DynamicBitSet.initEmpty(allocator, limit);
+    var multiples = ArrayList(u32).init(allocator);
     defer multiples.deinit();
     for (factors) |factor| {
-        if (factor == 0) {
-            continue;
-        }
-        var n = factor;
-        while (n < limit) : (n += factor) {
-            multiples.set(n);
+        if (factor > 0) {
+            var n = factor;
+            while (n < limit) : (n += factor) {
+                try multiples.append(n);
+            }
         }
     }
-    var iter = multiples.iterator(.{});
+    sort.heap(u32, multiples.items, {}, sort.asc(u32));
+
+    var prev: ?u32 = null;
     var result: u64 = 0;
-    while (iter.next()) |multiple| {
-        result += multiple;
+    for (multiples.items) |multiple| {
+        if (prev != multiple) {
+            result += multiple;
+            prev = multiple;
+        }
     }
     return result;
 }
