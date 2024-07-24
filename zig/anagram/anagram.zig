7d58c6ef9c91cddb54953f9fcdb6580860d169a4
Minsub Kim <rapidfsub@gmail.com>
2024-07-25 07:01:54 +0900

    solve anagram

--- zig/anagram/anagram.zig
+++ zig/anagram/anagram.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const ascii = std.ascii;
 const mem = std.mem;
 
 /// Returns the set of strings in `candidates` that are anagrams of `word`.
@@ -8,8 +9,30 @@ pub fn detectAnagrams(
     word: []const u8,
     candidates: []const []const u8,
 ) !std.BufSet {
-    _ = allocator;
-    _ = word;
-    _ = candidates;
-    @compileError("please implement the detectAnagrams function");
+    var tally = [_]u32{0} ** 26;
+    for (word) |letter| {
+        if (ascii.isAlphabetic(letter)) {
+            tally[ascii.toLower(letter) - 'a'] += 1;
+        }
+    }
+
+    var result = std.BufSet.init(allocator);
+    for (candidates) |candid| {
+        var tc = [_]u32{0} ** 26;
+        for (candid) |letter| {
+            if (ascii.isAlphabetic(letter)) {
+                tc[ascii.toLower(letter) - 'a'] += 1;
+            }
+        }
+
+        if (mem.eql(u32, &tc, &tally)) {
+            for (candid, word) |lhs, rhs| {
+                if (ascii.toLower(lhs) != ascii.toLower(rhs)) {
+                    try result.insert(candid);
+                    break;
+                }
+            }
+        }
+    }
+    return result;
 }
