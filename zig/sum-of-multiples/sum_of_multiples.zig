3252d7d89cd009b1d2be22fc245408a2a86ee1f9
Minsub Kim <rapidfsub@gmail.com>
2024-07-24 21:50:58 +0900

    refactor sum of multiples using DynamicBitSet

--- zig/sum-of-multiples/sum_of_multiples.zig
+++ zig/sum-of-multiples/sum_of_multiples.zig
@@ -1,16 +1,23 @@
 const std = @import("std");
 const mem = std.mem;
+const DynamicBitSet = std.bit_set.DynamicBitSet;
 
 pub fn sum(allocator: mem.Allocator, factors: []const u32, limit: u32) !u64 {
-    _ = allocator;
-    var result: u64 = 0;
-    for (1..limit) |n| {
-        for (factors) |factor| {
-            if (factor > 0 and @rem(n, factor) == 0) {
-                result += n;
-                break;
-            }
+    var multiples = try DynamicBitSet.initEmpty(allocator, limit);
+    defer multiples.deinit();
+    for (factors) |factor| {
+        if (factor == 0) {
+            continue;
         }
+        var n = factor;
+        while (n < limit) : (n += factor) {
+            multiples.set(n);
+        }
+    }
+    var iter = multiples.iterator(.{});
+    var result: u64 = 0;
+    while (iter.next()) |multiple| {
+        result += multiple;
     }
     return result;
 }
