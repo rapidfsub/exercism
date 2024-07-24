3e8ff42c05482d94c9794682e35f8971623affad
Minsub Kim <rapidfsub@gmail.com>
2024-07-24 19:42:00 +0900

    solve all your base

--- zig/all-your-base/all_your_base.zig
+++ zig/all-your-base/all_your_base.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
+const math = std.math;
 const mem = std.mem;
+const ArrayList = std.ArrayList;
 
 pub const ConversionError = error{
     InvalidInputBase,
@@ -15,9 +17,37 @@ pub fn convert(
     input_base: u32,
     output_base: u32,
 ) (mem.Allocator.Error || ConversionError)![]u32 {
-    _ = allocator;
-    _ = digits;
-    _ = input_base;
-    _ = output_base;
-    @compileError("please implement the convert function");
+    if (input_base < 2) {
+        return ConversionError.InvalidInputBase;
+    } else if (output_base < 2) {
+        return ConversionError.InvalidOutputBase;
+    }
+
+    var value: u32 = 0;
+    for (digits) |digit| {
+        if (digit < input_base) {
+            value *= input_base;
+            value += digit;
+        } else {
+            return ConversionError.InvalidDigit;
+        }
+    }
+
+    var buffer = ArrayList(u32).init(allocator);
+    defer buffer.deinit();
+    while (value > 0) : (value /= output_base) {
+        try buffer.append(@rem(value, output_base));
+    }
+
+    var result = ArrayList(u32).init(allocator);
+    defer result.deinit();
+    if (buffer.items.len > 0) {
+        var i = buffer.items.len;
+        while (i > 0) : (i -= 1) {
+            try result.append(buffer.items[i - 1]);
+        }
+    } else {
+        try result.append(0);
+    }
+    return result.toOwnedSlice();
 }
